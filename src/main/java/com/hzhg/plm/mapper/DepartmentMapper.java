546ba10fc81ec0504004ee8eb6895b18067521a4
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-30 15:12:18 +0800

    add: add DepartmentMapper, DepartmentService and DepartmentServiceImpl

---
+++ src/main/java/com/hzhg/plm/mapper/DepartmentMapper.java
@@ -0,0 +1,9 @@
+package com.hzhg.plm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hzhg.plm.entity.Department;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DepartmentMapper extends BaseMapper<Department> {
+}
---
+++ src/main/java/com/hzhg/plm/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hzhg.plm.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hzhg.plm.entity.Department;
+import com.hzhg.plm.mapper.DepartmentMapper;
+import com.hzhg.plm.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class DepartmentServiceImpl extends ServiceImpl<DepartmentMapper, Department> implements DepartmentService {
+}
