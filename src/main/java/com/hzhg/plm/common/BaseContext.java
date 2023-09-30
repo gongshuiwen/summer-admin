3e68ac2a5f8a6d3d9a65c26f2ca1055394901d09
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-30 14:09:36 +0800

    opt: 添加获取当前用户ID的工具类

---
+++ src/main/java/com/hzhg/plm/common/BaseContext.java
@@ -0,0 +1,15 @@
+package com.hzhg.plm.common;
+
+import com.hzhg.plm.entity.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class BaseContext {
+
+    public static Long getCurrentUserId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof User) {
+            return ((User) principal).getId();
+        }
+        return null;
+    }
+}
