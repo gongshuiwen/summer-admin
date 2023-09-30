e1dff2ffae2625357395dc800cf51d2814ab132f
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-30 15:09:20 +0800

    add: add RoleMapper, RoleService and RoleServiceImpl

---
+++ src/main/java/com/hzhg/plm/service/RoleService.java
@@ -0,0 +1,7 @@
+package com.hzhg.plm.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hzhg.plm.entity.Role;
+
+public interface RoleService extends IService<Role> {
+}
