e9920341fd358849d73da0543e71f3f29aadb369
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-30 14:10:09 +0800

    opt: 修改 BaseEntity

--- src/main/java/com/hzhg/plm/common/BaseEntity.java
+++ src/main/java/com/hzhg/plm/common/BaseEntity.java
@@ -3,10 +3,16 @@ package com.hzhg.plm.common;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class BaseEntity {
+
+@Getter
+@Setter
+public class BaseEntity implements Serializable {
 
     @Schema(name = "创建时间")
     @TableField(fill = FieldFill.INSERT)
@@ -18,9 +24,9 @@ public class BaseEntity {
 
     @Schema(name = "创建用户")
     @TableField(fill = FieldFill.INSERT)
-    private Long createUser;
+    private Long createBy;
 
     @Schema(name = "更新用户")
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Long updateUser;
+    private Long updateBy;
 }
