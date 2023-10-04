90f8b896a90dd18e2325eaaf5da15335c7204510
gongshuiwen <shuiwen.gong@outlook.com>
2023-10-04 16:42:42 +0800

    feat: implement user roles and authorities load from database

--- src/main/java/com/hzhg/plm/mapper/RoleMapper.java
+++ src/main/java/com/hzhg/plm/mapper/RoleMapper.java
@@ -3,7 +3,14 @@ package com.hzhg.plm.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hzhg.plm.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface RoleMapper extends BaseMapper<Role> {
+
+    @Select("SELECT * FROM role LEFT JOIN user_role ON role.id = user_role.role_id " +
+            "WHERE user_role.user_id = #{userId}")
+    List<Role> getRolesByUserId(Long userId);
 }
--- src/main/java/com/hzhg/plm/service/impl/UserServiceImpl.java
+++ src/main/java/com/hzhg/plm/service/impl/UserServiceImpl.java
@@ -2,26 +2,51 @@ package com.hzhg.plm.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hzhg.plm.entity.Role;
 import com.hzhg.plm.entity.User;
+import com.hzhg.plm.mapper.RoleMapper;
 import com.hzhg.plm.mapper.UserMapper;
 import com.hzhg.plm.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    @Autowired
+    RoleMapper roleMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(User::getUsername, username);
-        UserDetails user = this.getOne(wrapper);
+        User user = this.getOne(wrapper);
         if (user == null) {
             throw new UsernameNotFoundException("");
         }
+
+        // Set roles
+        List<Role> roles = roleMapper.getRolesByUserId(user.getId());
+        user.setRoles(roles);
+
+        // Set authorities
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(ROLE_PREFIX + role.getName()));
+        }
+        user.setAuthorities(authorities);
         return user;
     }
 }
