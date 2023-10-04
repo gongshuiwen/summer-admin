46091563f1fa335d89a3b6ce296ebfb35f31bea6
gongshuiwen <shuiwen.gong@outlook.com>
2023-10-04 16:40:36 +0800

    opt: optimize Role entity definition

--- src/main/java/com/hzhg/plm/entity/Role.java
+++ src/main/java/com/hzhg/plm/entity/Role.java
@@ -21,7 +21,7 @@ public class Role extends BaseEntity
     private Long id;
 
     @Schema(description = "角色名称")
-    private String roleName;
+    private String name;
 
     @Schema(description = "显示顺序")
     private Integer orderNum;
@@ -30,25 +30,7 @@ public class Role extends BaseEntity
     private String status;
 
     @Schema(description = "角色权限")
-    private String roleKey;
-
-    @Schema(description = "数据范围 1=所有数据权限,2=自定义数据权限,3=本部门数据权限,4=本部门及以下数据权限,5=仅本人数据权限")
-    private String dataScope;
-
-    @Schema(description = "菜单树选择项是否关联显示 0=父子不互相关联显示,1=父子互相关联显示")
-    private boolean menuCheckStrictly;
-
-    @Schema(description = "部门树选择项是否关联显示 0=父子不互相关联显示,1=父子互相关联显示")
-    private boolean deptCheckStrictly;
-
-    @Schema(description = "菜单组")
-    private Long[] menuIds;
-
-    @Schema(description = "部门组")
-    private Long[] deptIds;
-
-    @Schema(description = "角色菜单权限")
-    private Set<String> permissions;
+    private String rolePerms;
 
     public boolean isAdmin() {
         return isAdmin(this.id);
@@ -60,8 +42,8 @@ public class Role extends BaseEntity
 
     @NotBlank(message = "角色名称不能为空")
     @Size(max = 30, message = "角色名称长度不能超过30个字符")
-    public String getRoleName() {
-        return roleName;
+    public String getName() {
+        return name;
     }
 
     @NotNull(message = "显示顺序不能为空")
@@ -71,7 +53,7 @@ public class Role extends BaseEntity
 
     @NotBlank(message = "权限字符不能为空")
     @Size(max = 100, message = "权限字符长度不能超过100个字符")
-    public String getRoleKey() {
-        return roleKey;
+    public String getRolePerms() {
+        return rolePerms;
     }
 }
