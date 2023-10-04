55e5b43e17c40ba46ff00d4c7a3f9ed44e98ae8e
gongshuiwen <shuiwen.gong@outlook.com>
2023-10-04 14:34:17 +0800

    feat: add WebSecurityConfig, implement simple authentication and authorization functions.

---
+++ src/main/java/com/hzhg/plm/config/WebSecurityConfig.java
@@ -0,0 +1,130 @@
+package com.hzhg.plm.config;
+
+import com.alibaba.fastjson.JSON;
+import com.hzhg.plm.common.R;
+import com.hzhg.plm.exception.BusinessExceptionEnum;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            DaoAuthenticationProvider daoAuthenticationProvider
+        ) throws Exception {
+
+        http
+                // Disable CSRF protection for separation architecture
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // Config form login filter
+                .formLogin(configurer -> configurer
+                        .successHandler(new CustomAuthenticationSuccessHandler())
+                        .failureHandler(new CustomAuthenticationFailureHandler()))
+
+                // Config logout filter
+                .logout(configurer -> configurer
+                        .logoutSuccessHandler(new CustomLogoutSuccessHandler()))
+
+                // Config exception handle filter
+                .exceptionHandling(configurer -> configurer
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                        .accessDeniedHandler(new CustomAccessDeniedHandler()))
+
+                // Static configurations for requests authorization
+                .authorizeHttpRequests(configurer -> configurer
+                        .mvcMatchers("/doc.html").permitAll()
+                        .mvcMatchers("/webjars/**").permitAll()
+                        .mvcMatchers("/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            response.getWriter().write(JSON.toJSONString(R.success(userDetails)));
+        }
+    }
+
+    private static class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(JSON.toJSONString(R.error(BusinessExceptionEnum.ERROR_AUTHENTICATION_FAILED)));
+        }
+    }
+
+    private static class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+
+        @Override
+        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(JSON.toJSONString(R.success("Logout Success!")));
+        }
+    }
+
+    private static class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+        @Override
+        public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException{
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(JSON.toJSONString(R.error(BusinessExceptionEnum.ERROR_AUTHENTICATION_FAILED)));
+        }
+    }
+
+    private static class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(JSON.toJSONString(R.error(BusinessExceptionEnum.ERROR_ACCESS_DENIED)));
+        }
+    }
+}
--- src/main/java/com/hzhg/plm/exception/BusinessExceptionEnum.java
+++ src/main/java/com/hzhg/plm/exception/BusinessExceptionEnum.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum BusinessExceptionEnum {
 
+    ERROR_AUTHENTICATION_FAILED(80001, "身份认证失败！"),
+    ERROR_ACCESS_DENIED(80002, "权限验证失败，无当前资源的访问权限！"),
+
     ERROR_TEST(90000, "This error is for test!"),
     ERROR_API_NOT_IMPLEMENTED(90001, "API is not implemented!"),
     ERROR_API_DEPRECATED(90002, "API is deprecated!"),
