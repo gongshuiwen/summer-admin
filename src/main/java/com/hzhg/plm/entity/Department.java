1ac0b52d16de76e5be191aec5607f2e5e85e3b72
gongshuiwen <shuiwen.gong@outlook.com>
2023-10-05 21:18:52 +0800

    opt: optimize Department entity definitions

--- src/main/java/com/hzhg/plm/entity/Department.java
+++ src/main/java/com/hzhg/plm/entity/Department.java
@@ -1,7 +1,7 @@
 package com.hzhg.plm.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.hzhg.plm.common.BaseEntity;
+import com.hzhg.plm.common.TreeBaseEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,32 +15,21 @@ import java.util.List;
 @Getter
 @Setter
 @Schema(description = "部门信息")
-public class Department extends BaseEntity {
+public class Department extends TreeBaseEntity {
 
     private static final long serialVersionUID = 1L;
 
     @Schema(description = "部门ID")
     private Long id;
 
-    @Schema(description = "部门名称")
-    private String name;
-
-    @Schema(description = "父部门ID")
-    private Long parentId;
-
-    @Schema(description = "父部门名称")
-    @TableField(exist = false)
-    private String parentName;
-
     @Schema(description = "显示顺序")
     private Integer orderNum;
 
-    @Schema(description = "部门状态 0=正常,1=停用")
+    @Schema(description = "状态 0=正常,1=停用")
     private String status;
 
-    @Schema(description = "子部门列表")
-    @TableField(exist = false)
-    private List<Department> children = new ArrayList<>();
+    @Schema(description = "部门名称")
+    private String name;
 
     @NotBlank(message = "部门名称不能为空")
     @Size(max = 30, message = "部门名称长度不能超过30个字符")
