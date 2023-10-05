df76405a21b75e69bc62317764390ba158d56c8b
gongshuiwen <shuiwen.gong@outlook.com>
2023-10-05 21:19:26 +0800

    opt: optimize Menu entity definitions

--- src/main/java/com/hzhg/plm/entity/Menu.java
+++ src/main/java/com/hzhg/plm/entity/Menu.java
@@ -1,6 +1,6 @@
 package com.hzhg.plm.entity;
 
-import com.hzhg.plm.common.BaseEntity;
+import com.hzhg.plm.common.TreeBaseEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,33 +8,28 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 @Schema(description = "菜单信息")
-public class Menu extends BaseEntity {
+public class Menu extends TreeBaseEntity {
 
     private static final long serialVersionUID = 1L;
 
     @Schema(description = "菜单ID")
     private Long id;
 
-    @Schema(description = "菜单名称")
-    private String name;
-
-    @Schema(description = "父菜单ID")
-    private Long parentId;
-
-    @Schema(description = "父菜单名称")
-    private String parentName;
-
     @Schema(description = "显示顺序")
     private Integer orderNum;
 
-    @Schema(description = "菜单状态 0=正常,1=停用")
+    @Schema(description = "状态 0=正常,1=停用")
     private String status;
+
+    @Schema(description = "菜单名称")
+    private String name;
+
+    @Schema(description = "菜单标题")
+    private String title;
 
     @Schema(description = "路由地址")
     private String path;
@@ -45,26 +40,11 @@ public class Menu extends BaseEntity {
     @Schema(description = "路由参数")
     private String query;
 
-    @Schema(description = "是否外链")
-    private String isFrame;
-
-    @Schema(description = "是否缓存（0缓存 1不缓存）")
-    private String isCache;
-
-    @Schema(description = "类型（M目录 C菜单 F按钮）")
-    private String menuType;
-
-    @Schema(description = "显示状态（0显示 1隐藏） ")
-    private String visible;
-
-    @Schema(description = "权限字符串")
-    private String perms;
+    @Schema(description = "外链地址")
+    private String url;
 
     @Schema(description = "菜单图标")
     private String icon;
-
-    @Schema(description = "子菜单")
-    private List<Menu> children = new ArrayList<>();
 
     @NotBlank(message = "菜单名称不能为空")
     @Size(max = 50, message = "菜单名称长度不能超过50个字符")
@@ -85,15 +65,5 @@ public class Menu extends BaseEntity {
     @Size(max = 200, message = "组件路径不能超过255个字符")
     public String getComponent() {
         return component;
-    }
-
-    @NotBlank(message = "菜单类型不能为空")
-    public String getMenuType() {
-        return menuType;
-    }
-
-    @Size(max = 100, message = "权限标识长度不能超过100个字符")
-    public String getPerms() {
-        return perms;
     }
 }
