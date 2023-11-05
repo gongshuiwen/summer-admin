e45b96f9aec405b014c26ea5dc34419d281785a3
gongshuiwen <shuiwen.gong@outlook.com>
2023-11-05 23:07:28 +0800

    feat: add accessDeniedExceptionHandler

--- src/main/java/com/hzhg/plm/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hzhg/plm/core/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package com.hzhg.plm.core.exception;
 
 import com.hzhg.plm.core.protocal.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.hzhg.plm.core.exception.BusinessExceptionEnum.ERROR_ACCESS_DENIED;
 
 
 @Slf4j
@@ -21,6 +24,11 @@ public class GlobalExceptionHandler {
         String message = e.getMessage();
         log.warn(String.format(_BUSINESS_ERROR_MESSAGE_TEMPLATE, code, message));
         return R.error(code, message);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public R<String> accessDeniedExceptionHandler(Exception e) {
+        return R.error(ERROR_ACCESS_DENIED);
     }
 
     @ExceptionHandler(Exception.class)
