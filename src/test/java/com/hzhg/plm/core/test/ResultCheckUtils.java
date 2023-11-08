b299a5c7812c95e2ebb79c53fd77f6d1e5a93a60
gongshuiwen <shuiwen.gong@outlook.com>
2023-11-08 20:46:43 +0800

    test: add ResultCheckUtils

---
+++ src/test/java/com/hzhg/plm/core/test/ResultCheckUtils.java
@@ -0,0 +1,28 @@
+package com.hzhg.plm.core.test;
+
+import com.hzhg.plm.core.exception.BusinessExceptionEnum;
+import com.hzhg.plm.core.protocal.R;
+import org.hamcrest.core.Is;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+public class ResultCheckUtils {
+
+    public static void checkResultActionsSuccess(ResultActions resultActions) throws Exception {
+        resultActions.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(R.SUCCESS_CODE)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Is.is(R.SUCCESS_MESSAGE)));
+    }
+
+    public static void checkResultActionsAccessDined(ResultActions resultActions) throws Exception {
+        resultActions.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(BusinessExceptionEnum.ERROR_ACCESS_DENIED.getCode())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Is.is(BusinessExceptionEnum.ERROR_ACCESS_DENIED.getMessage())));
+    }
+}
