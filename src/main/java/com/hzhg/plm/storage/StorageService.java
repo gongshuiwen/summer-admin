0e6312750901f6adcb2f1f63fc961403727fbb2d
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-05 21:00:39 +0800

    feat: add StorageService and its implements: FileStorageService and MinioStorageService

---
+++ src/main/java/com/hzhg/plm/storage/StorageService.java
@@ -0,0 +1,13 @@
+package com.hzhg.plm.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+    void init() throws Exception;
+
+    String store(MultipartFile multipartFile) throws Exception;
+
+    Resource loadAsResource(String filename) throws Exception;
+}
