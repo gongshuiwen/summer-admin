54395e2b572c3ecea5832086b1ef2e021239a2f7
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-05 23:46:09 +0800

    test: add tests for RoleService

---
+++ src/test/java/com/hzhg/plm/service/TestRoleService.java
@@ -0,0 +1,36 @@
+package com.hzhg.plm.service;
+
+import com.hzhg.plm.entity.Permission;
+import com.hzhg.plm.entity.Role;
+import com.hzhg.plm.service.impl.RoleServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+@Sql(scripts = {
+        "/sql/ddl/role.sql",
+        "/sql/ddl/user.sql",
+        "/sql/ddl/user_role.sql",
+        "/sql/test/data/role.sql",
+        "/sql/test/data/user.sql",
+        "/sql/test/data/user_role.sql",
+})
+public class TestRoleService {
+
+    @Autowired
+    RoleService roleService;
+
+    @Test
+    public void testGetRolesByUserId() {
+        Set<Role> roles = roleService.getRolesByUserId(1L);
+        Set<String> rolesExpect = new HashSet<>(
+                Arrays.asList("SYS_ADMIN", "PLM_ADMIN"));
+        Assertions.assertEquals(rolesExpect, roles.stream().map(Role::getCode).collect(Collectors.toSet()));
+    }
+}
