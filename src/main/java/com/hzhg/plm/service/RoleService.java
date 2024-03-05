4125545074a001c6d880074b0968534f7e507553
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-05 23:42:20 +0800

    feat: add getRolesByUserId method in RoleService and its implement

--- src/main/java/com/hzhg/plm/service/RoleService.java
+++ src/main/java/com/hzhg/plm/service/RoleService.java
@@ -3,5 +3,9 @@ package com.hzhg.plm.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hzhg.plm.entity.Role;
 
+import java.util.Set;
+
 public interface RoleService extends IService<Role> {
+
+    Set<Role> getRolesByUserId(Long userId);
 }
--- src/main/java/com/hzhg/plm/service/impl/RoleServiceImpl.java
+++ src/main/java/com/hzhg/plm/service/impl/RoleServiceImpl.java
@@ -5,9 +5,23 @@ import com.hzhg.plm.entity.Role;
 import com.hzhg.plm.mapper.RoleMapper;
 import com.hzhg.plm.service.RoleService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Slf4j
 @Service
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, Role> implements RoleService {
+
+    @Autowired
+    RoleMapper roleMapper;
+
+    @Override
+    public Set<Role> getRolesByUserId(Long userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException();
+        }
+        return roleMapper.getRolesByUserId(userId);
+    }
 }
