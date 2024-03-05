7a1487ecf26c19228f782703deba5d4f79fa9a92
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-05 23:31:36 +0800

    opt: optimize PermissionMapper's getPermissionsByRoleId method

--- src/main/java/com/hzhg/plm/mapper/PermissionMapper.java
+++ src/main/java/com/hzhg/plm/mapper/PermissionMapper.java
@@ -2,16 +2,12 @@ package com.hzhg.plm.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hzhg.plm.entity.Permission;
-import com.hzhg.plm.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
+import java.util.Set;
 
 @Mapper
 public interface PermissionMapper extends BaseMapper<Permission> {
 
-    @Select("SELECT * FROM permission LEFT JOIN role_permission ON permission.id = role_permission.perm_id " +
-            "WHERE role_permission.role_id in #{roleIds}")
-    List<Permission> getPermissionsByRoleIds(List<Long> roleIds);
+    Set<Permission> getPermissionsByRoleId(Long roleId);
 }
