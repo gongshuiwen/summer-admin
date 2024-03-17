2b9265dce112acd72003a425bd5b116eb6e98b53
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-17 21:12:13 +0800

    opt: optimize Department entity definition

--- src/main/java/com/hzhg/plm/entity/Department.java
+++ src/main/java/com/hzhg/plm/entity/Department.java
@@ -16,28 +16,19 @@ public class Department extends TreeBaseEntity<Department> {
 
     private static final long serialVersionUID = 1L;
 
+    @Schema(description = "部门名称")
+    @NotBlank(message = "部门名称不能为空")
+    @Size(max = 18, message = "部门名称长度不能超过18个字符")
+    private String name;
+
     @Schema(description = "显示顺序")
     private Integer orderNum;
 
     @Schema(description = "状态 1=正常,0=停用")
     private Integer status;
 
-    @Schema(description = "部门名称")
-    private String name;
-
     @Override
     public String getDisplayName() {
         return getName();
-    }
-
-    @NotBlank(message = "部门名称不能为空")
-    @Size(max = 30, message = "部门名称长度不能超过30个字符")
-    public String getName() {
-        return name;
-    }
-
-    @NotNull(message = "显示顺序不能为空")
-    public Integer getOrderNum() {
-        return orderNum;
     }
 }
