37ab83d8d2514d346c1141e7af17be6413106f2d
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-17 19:27:23 +0800

    feat: integrate with spring validation, add ExceptionHandler and BusinessExceptionEnum for MethodArgumentNotValidException

--- src/main/java/com/hzhg/plm/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hzhg/plm/core/exception/GlobalExceptionHandler.java
@@ -3,12 +3,16 @@ package com.hzhg.plm.core.exception;
 import com.hzhg.plm.core.protocal.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static com.hzhg.plm.core.exception.BusinessExceptionEnum.ERROR_ACCESS_DENIED;
-import static com.hzhg.plm.core.exception.BusinessExceptionEnum.ERROR_INTERNAL;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.hzhg.plm.core.exception.BusinessExceptionEnum.*;
 
 
 @Slf4j
@@ -28,6 +32,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccessDeniedException.class)
     public R<String> accessDeniedExceptionHandler(Exception e) {
         return R.error(ERROR_ACCESS_DENIED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public R<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return R.error(ERROR_INVALID_ARGUMENTS, errors);
     }
 
     @ExceptionHandler(Exception.class)
