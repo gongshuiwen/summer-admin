bf66eb6b41e54a2f7687f45edbc52888fc0294f1
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-17 21:02:33 +0800

    opt: optimize Permission entity definition

--- src/main/java/com/hzhg/plm/entity/Permission.java
+++ src/main/java/com/hzhg/plm/entity/Permission.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @Schema(description = "权限信息")
@@ -14,9 +17,13 @@ public class Permission  extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
     @Schema(description = "权限标识")
+    @NotBlank(message = "权限标识不能为空")
+    @Size(max = 18, message = "权限标识长度不能超过18个字符")
     private String code;
 
     @Schema(description = "权限名称")
+    @NotBlank(message = "权限名称不能为空")
+    @Size(max = 18, message = "权限名称长度不能超过18个字符")
     private String name;
 
     @Schema(description = "显示顺序")
