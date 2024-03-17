a9a6dff8d48f0942736e0096bb0888926183b293
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-17 21:10:46 +0800

    opt: optimize ddl for table department, user

--- src/main/resources/sql/ddl/department.sql
+++ src/main/resources/sql/ddl/department.sql
@@ -7,7 +7,7 @@ CREATE TABLE `department` (
     `update_user`   bigint          NOT NULL    DEFAULT 0                   COMMENT '更新用户',
     `parent_id`     bigint          NOT NULL    DEFAULT 0                   COMMENT '父级ID',
     `parent_path`   varchar(50)     NOT NULL    DEFAULT ''                  COMMENT '祖级路径',
-    `name`          varchar(50)     NOT NULL    DEFAULT ''                  COMMENT '部门名称',
+    `name`          varchar(18)     NOT NULL    DEFAULT ''                  COMMENT '部门名称',
     `order_num`     smallint        NOT NULL    DEFAULT 0                   COMMENT '显示顺序',
     `status`        tinyint         NOT NULL    DEFAULT 1                   COMMENT '状态 1=正常,0=停用',
     PRIMARY KEY (`id`)
