4d62d8d8a33ac9b95e5a58a9117ba6c331578db9
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-23 14:41:33 +0800

    opt: add static fields in DataAccessAuthority and add toString method

--- src/main/java/com/hzhg/plm/core/security/DataAccessAuthority.java
+++ src/main/java/com/hzhg/plm/core/security/DataAccessAuthority.java
@@ -12,5 +12,15 @@ public enum DataAccessAuthority {
     UPDATE("UPDATE"),
     DELETE("DELETE");
 
+    public static final String AUTHORITY_SELECT = "SELECT";
+    public static final String AUTHORITY_CREATE = "CREATE";
+    public static final String AUTHORITY_UPDATE = "UPDATE";
+    public static final String AUTHORITY_DELETE = "DELETE";
+
     private final String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
