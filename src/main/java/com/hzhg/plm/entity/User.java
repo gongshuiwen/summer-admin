81185dcfdf126fc14251428a3d66da4fc340a250
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-27 23:19:54 +0800

    opt: optimize the validation annotations for User entity's fields

--- src/main/java/com/hzhg/plm/entity/User.java
+++ src/main/java/com/hzhg/plm/entity/User.java
@@ -4,72 +4,88 @@ package com.hzhg.plm.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hzhg.plm.core.annotations.AllowedForRoles;
 import com.hzhg.plm.core.annotations.FetchName;
 import com.hzhg.plm.core.entity.BaseEntity;
-import com.hzhg.plm.core.annotations.AllowedForRoles;
+import com.hzhg.plm.core.validation.CreateValidationGroup;
+import com.hzhg.plm.core.validation.NullOrNotBlank;
+import com.hzhg.plm.core.validation.UpdateValidationGroup;
 import com.hzhg.plm.mapper.DepartmentMapper;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+
+import static com.hzhg.plm.core.security.DataAccessAuthorityChecker.ROLE_ADMIN;
 
 @Getter
 @Setter
 @Schema(description = "用户信息")
 public class User extends BaseEntity implements Serializable, UserDetails {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private static final String ROLE_PREFIX = "ROLE_";
 
     @Schema(description = "用户名")
-    @NotBlank(message = "用户名不能为空")
-    @Size(max = 30, message = "用户名长度不能超过30个字符")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(min = 2, max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String username;
 
+    @Schema(description = "昵称")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(min = 2, max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private String nickname;
+
     @Schema(description = "密码")
-    @NotBlank(message = "用户密码不能为空")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(min = 6, max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    @Schema(description = "昵称")
-    @Size(max = 30, message = "昵称长度不能超过30个字符")
-    private String nickname;
-
     @Schema(description = "邮箱")
-    @Email(message = "邮箱格式不正确")
-    @Size(max = 50, message = "邮箱长度不能超过50个字符")
+    @Email(groups = {CreateValidationGroup.class, UpdateValidationGroup.class},
+            message = "邮箱格式不正确")
+    @Size(max = 50, groups = {CreateValidationGroup.class, UpdateValidationGroup.class},
+            message = "邮箱长度不能超过50个字符")
     private String email;
 
     @Schema(description = "手机号码")
-    @Size(max = 11, message = "手机号码长度不能超过11个字符")
+    @Size(max = 11, groups = {CreateValidationGroup.class, UpdateValidationGroup.class},
+            message = "手机号码长度不能超过11个字符")
     private String phone;
 
     @Schema(description = "用户性别 0=未知,1=女,2=男")
+    @Min(value = 0, groups = {CreateValidationGroup.class, UpdateValidationGroup.class}, message = "性别不正确")
+    @Max(value = 2, groups = {CreateValidationGroup.class, UpdateValidationGroup.class}, message = "性别不正确")
     private Integer sex;
 
     @Schema(description = "用户头像")
     private String avatar;
 
     @Schema(description = "状态 1=正常,0=停用")
+    @AllowedForRoles(value = {ROLE_ADMIN})
     private Integer status;
 
-    @AllowedForRoles(value = {"SYS_ADMIN"})
     @Schema(description = "最后登录IP")
+    @AllowedForRoles(value = {ROLE_ADMIN})
     private String loginIp;
 
-    @AllowedForRoles(value = {"SYS_ADMIN"})
     @Schema(description = "最后登录时间")
+    @AllowedForRoles(value = {ROLE_ADMIN})
     private LocalDateTime loginTime;
 
     @Schema(description = "部门ID")
