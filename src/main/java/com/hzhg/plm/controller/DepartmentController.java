1b6dca9f7151691c94eff24d28a1949fc59b958d
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-27 22:32:52 +0800

    deprecate: remove unused api in RoleController and DepartmentController

--- src/main/java/com/hzhg/plm/controller/DepartmentController.java
+++ src/main/java/com/hzhg/plm/controller/DepartmentController.java
@@ -1,25 +1,15 @@
 package com.hzhg.plm.controller;
 
 import com.hzhg.plm.core.controller.BaseController;
-import com.hzhg.plm.core.protocal.R;
 import com.hzhg.plm.entity.Department;
 import com.hzhg.plm.service.DepartmentService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @Tag(name = "部门接口")
 @RequestMapping("/department")
 public class DepartmentController extends BaseController<DepartmentService, Department> {
-
-    @Operation(summary = "查询部门列表")
-    @GetMapping
-    public R<List<Department>> list() {
-        return R.success(service.list());
-    }
 }
--- src/main/java/com/hzhg/plm/controller/RoleController.java
+++ src/main/java/com/hzhg/plm/controller/RoleController.java
@@ -1,18 +1,10 @@
 package com.hzhg.plm.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hzhg.plm.core.controller.BaseController;
-import com.hzhg.plm.core.protocal.R;
 import com.hzhg.plm.entity.Role;
 import com.hzhg.plm.service.RoleService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -20,21 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "角色接口")
 @RequestMapping("/role")
 public class RoleController extends BaseController<RoleService, Role> {
-
-    @Operation(summary = "分页查询")
-    @GetMapping
-    public R<IPage<Role>> page(
-            @Nullable @RequestParam Long pageNum,
-            @Nullable @RequestParam Long pageSize,
-            @Nullable @RequestParam String name,
-            @Nullable @RequestParam String code,
-            @Nullable @RequestParam Integer status
-    ) {
-        IPage<Role> page = new Page<>(pageNum == null ? 1 : pageNum, pageSize == null ? 20 : pageSize);
-        LambdaQueryWrapper<Role> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.orderByAsc(Role::getOrderNum);
-        queryWrapper.like(name != null, Role::getName, name);
-        queryWrapper.eq(status != null, Role::getStatus, status);
-        return R.success(service.page(page, queryWrapper));
-    }
 }
