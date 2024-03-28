6f75caf7af9c70ad9f0e2e6438ae84b38449d074
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-28 22:22:36 +0800

    test: update test data

--- src/test/resources/sql/test/data/role.sql
+++ src/test/resources/sql/test/data/role.sql
@@ -4,7 +4,8 @@
 INSERT INTO role
 (id, code, name)
 VALUES
-(1, 'SYS_ADMIN',    '系统管理员'),
-(2, 'PLM_ADMIN',    '管理员'),
-(3, 'PLM_DESIGN',   '设计人员'),
-(4, 'PLM_PROCESS',  '工艺人员');
+(1,     'SYS_ADMIN',    '系统管理员'),
+(2,     'BASE_USER',    '基础用户'),
+(101,   'PLM_ADMIN',    '管理员'),
+(102,   'PLM_DESIGN',   '设计人员'),
+(103,   'PLM_PROCESS',  '工艺人员');
--- src/test/resources/sql/test/data/role_permission.sql
+++ src/test/resources/sql/test/data/role_permission.sql
@@ -4,7 +4,5 @@
 INSERT INTO `role_permission`
 (role_id, perm_id)
 VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4);
+(2, 1),
+(2, 3);
