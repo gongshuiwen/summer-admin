4f02b5a503fe05047a9febf82d1aab230f34e763
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-30 22:04:32 +0800

    opt: optimize Department entity and ddl definition

--- src/main/java/com/hzhg/plm/entity/Department.java
+++ src/main/java/com/hzhg/plm/entity/Department.java
@@ -1,30 +1,38 @@
 package com.hzhg.plm.entity;
 
 import com.hzhg.plm.core.entity.TreeBaseEntity;
+import com.hzhg.plm.core.validation.CreateValidationGroup;
+import com.hzhg.plm.core.validation.NullOrNotBlank;
+import com.hzhg.plm.core.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.io.Serial;
+
 @Getter
 @Setter
 @Schema(description = "部门信息")
 public class Department extends TreeBaseEntity<Department> {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
-    @Schema(description = "部门名称")
-    @NotBlank(message = "部门名称不能为空")
-    @Size(max = 18, message = "部门名称长度不能超过18个字符")
-    private String name;
+    @Schema(description = "状态 1=正常,0=停用")
+    private Integer status;
 
     @Schema(description = "显示顺序")
     private Integer orderNum;
 
-    @Schema(description = "状态 1=正常,0=停用")
-    private Integer status;
+    @Schema(description = "部门名称")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(min = 2, max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private String name;
 
     @Override
     public String getDisplayName() {
--- src/main/resources/sql/ddl/department.sql
+++ src/main/resources/sql/ddl/department.sql
@@ -5,10 +5,10 @@ CREATE TABLE `department` (
     `update_time`   datetime        NOT NULL    DEFAULT CURRENT_TIMESTAMP   COMMENT '更新时间',
     `create_user`   bigint          NOT NULL    DEFAULT 0                   COMMENT '创建用户',
     `update_user`   bigint          NOT NULL    DEFAULT 0                   COMMENT '更新用户',
-    `status`        tinyint         NOT NULL    DEFAULT 1                   COMMENT '状态 1=正常,0=停用',
     `parent_id`     bigint          NOT NULL    DEFAULT 0                   COMMENT '父级ID',
     `parent_path`   varchar(50)     NOT NULL    DEFAULT ''                  COMMENT '祖级路径',
-    `name`          varchar(18)     NOT NULL    DEFAULT ''                  COMMENT '部门名称',
+    `status`        tinyint         NOT NULL    DEFAULT 1                   COMMENT '状态 1=正常,0=停用',
     `order_num`     smallint        NOT NULL    DEFAULT 0                   COMMENT '显示顺序',
+    `name`          varchar(18)     NOT NULL    DEFAULT ''                  COMMENT '部门名称',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='部门信息';
