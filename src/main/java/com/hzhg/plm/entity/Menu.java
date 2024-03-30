d949b3f784e7036f5b0dc4fb50b965ba110daea5
gongshuiwen <shuiwen.gong@outlook.com>
2024-03-30 22:02:59 +0800

    opt: optimize Menu entity and ddl definition

--- src/main/java/com/hzhg/plm/entity/Menu.java
+++ src/main/java/com/hzhg/plm/entity/Menu.java
@@ -1,70 +1,66 @@
 package com.hzhg.plm.entity;
 
 import com.hzhg.plm.core.entity.TreeBaseEntity;
+import com.hzhg.plm.core.validation.CreateValidationGroup;
+import com.hzhg.plm.core.validation.NullOrNotBlank;
+import com.hzhg.plm.core.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.io.Serial;
 
 @Getter
 @Setter
 @Schema(description = "菜单信息")
 public class Menu extends TreeBaseEntity<Menu> {
 
+    @Serial
     private static final long serialVersionUID = 1L;
-
-    @Schema(description = "显示顺序")
-    private Integer orderNum;
 
     @Schema(description = "状态 1=正常,0=停用")
     private Integer status;
 
+    @Schema(description = "显示顺序")
+    private Integer orderNum;
+
     @Schema(description = "菜单名称")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(min = 2, max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String name;
 
     @Schema(description = "菜单标题")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(min = 2, max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String title;
 
     @Schema(description = "路由地址")
+    @NotNull(groups = {CreateValidationGroup.class})
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String path;
 
+    @Schema(description = "菜单图标")
+    @Size(max = 18, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private String icon;
+
     @Schema(description = "组件路径")
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(max = 50, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String component;
 
-    @Schema(description = "路由参数")
-    private String query;
-
-    @Schema(description = "外链地址")
-    private String url;
-
-    @Schema(description = "菜单图标")
-    private String icon;
+    @Schema(description = "重定向")
+    @NullOrNotBlank(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    @Size(max = 50, groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
+    private String redirect;
 
     @Override
     public String getDisplayName() {
-        return getName();
-    }
-
-    @NotBlank(message = "菜单名称不能为空")
-    @Size(max = 50, message = "菜单名称长度不能超过50个字符")
-    public String getName() {
-        return name;
-    }
-
-    @NotNull(message = "显示顺序不能为空")
-    public Integer getOrderNum() {
-        return orderNum;
-    }
-
-    @Size(max = 200, message = "路由地址不能超过200个字符")
-    public String getPath() {
-        return path;
-    }
-
-    @Size(max = 200, message = "组件路径不能超过255个字符")
-    public String getComponent() {
-        return component;
+        return getTitle();
     }
 }
