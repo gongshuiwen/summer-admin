f117738bcc7ed64ae834ad23764e67d12d8af4b0
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-02 20:05:00 +0800

    opt: optimize UserRoleMapper and RolePermissionMapper to RelationMapper

--- src/main/java/com/hzhg/plm/mapper/RolePermissionMapper.java
+++ src/main/java/com/hzhg/plm/mapper/RolePermissionMapper.java
@@ -1,11 +1,18 @@
 package com.hzhg.plm.mapper;
 
+import com.hzhg.plm.core.mapper.MapperRelation;
+import com.hzhg.plm.core.mapper.RelationMapper;
+import com.hzhg.plm.entity.Permission;
+import com.hzhg.plm.entity.Role;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Set;
 
 @Mapper
-public interface RolePermissionMapper {
+@MapperRelation(table = "role_permission",
+        field1 = "role_id", class1 = Role.class,
+        field2 = "perm_id", class2 = Permission.class)
+public interface RolePermissionMapper extends RelationMapper {
 
     default Set<Long> getPermissionIdsByRoleId(Long roleId) {
         return getPermissionIdsByRoleIds(Set.of(roleId));
--- src/main/java/com/hzhg/plm/mapper/UserRoleMapper.java
+++ src/main/java/com/hzhg/plm/mapper/UserRoleMapper.java
@@ -1,11 +1,18 @@
 package com.hzhg.plm.mapper;
 
+import com.hzhg.plm.core.mapper.MapperRelation;
+import com.hzhg.plm.core.mapper.RelationMapper;
+import com.hzhg.plm.entity.Role;
+import com.hzhg.plm.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Set;
 
 @Mapper
-public interface UserRoleMapper {
+@MapperRelation(table = "user_role",
+        field1 = "user_id", class1 = User.class,
+        field2 = "role_id", class2 = Role.class)
+public interface UserRoleMapper extends RelationMapper {
 
     default Set<Long> getRoleIdsByUserId(Long userId) {
         return getRoleIdsByUserIds(Set.of(userId));
