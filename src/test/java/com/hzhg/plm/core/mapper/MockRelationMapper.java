db57e35e5fb7b289603a467e34e72563b7015b3f
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-02 19:59:08 +0800

    test: update MockRelationMapper

--- src/test/java/com/hzhg/plm/core/mapper/MockRelationMapper.java
+++ src/test/java/com/hzhg/plm/core/mapper/MockRelationMapper.java
@@ -5,6 +5,8 @@ import com.hzhg.plm.core.entity.Mock2;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-@MapperRelation(table = "mock_relation", field1 = "mock1_id", field2 = "mock2_id")
-public interface MockRelationMapper extends RelationMapper<Mock1, Mock2> {
+@MapperRelation(table = "mock_relation",
+        field1 = "mock1_id", class1 = Mock1.class,
+        field2 = "mock2_id", class2 = Mock2.class)
+public interface MockRelationMapper extends RelationMapper {
 }
