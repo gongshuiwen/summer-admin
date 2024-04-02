2c91524a595225c2f89c4bdf2749093e66d3b006
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-02 19:59:54 +0800

    test: add Mock1Mapper, Mock2Mapper, Mock1Service, Mock2Service, Mock1ServiceImpl, Mock2ServiceImpl

---
+++ src/test/java/com/hzhg/plm/core/service/impl/Mock2ServiceImpl.java
@@ -0,0 +1,11 @@
+package com.hzhg.plm.core.service.impl;
+
+import com.hzhg.plm.core.entity.Mock2;
+import com.hzhg.plm.core.mapper.Mock2Mapper;
+import com.hzhg.plm.core.service.AbstractBaseService;
+import com.hzhg.plm.core.service.Mock2Service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Mock2ServiceImpl extends AbstractBaseService<Mock2Mapper, Mock2> implements Mock2Service {
+}
