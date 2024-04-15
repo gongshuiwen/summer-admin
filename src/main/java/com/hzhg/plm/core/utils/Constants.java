d76188a427549e7ffa8543d69f1c83a9cc3390a7
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-15 20:45:25 +0800

    opt: add Constants class

---
+++ src/main/java/com/hzhg/plm/core/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.hzhg.plm.core.utils;
+
+public class Constants {
+
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final String CODE_SYS_ADMIN = "SYS_ADMIN";
+    public static final String ROLE_SYS_ADMIN = ROLE_PREFIX + CODE_SYS_ADMIN;
+    public static final String CODE_BASE_USER = "BASE_USER";
+    public static final String ROLE_BASE_USER = ROLE_PREFIX + CODE_BASE_USER;
+
+    private Constants() {}
+}
