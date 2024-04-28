27ef5e8c5c374ee0b7b88537f4223c1a41965f02
gongshuiwen <shuiwen.gong@outlook.com>
2024-04-28 21:17:14 +0800

    test: update tests for PermissionService

--- src/test/java/com/hzhg/plm/service/TestPermissionService.java
+++ src/test/java/com/hzhg/plm/service/TestPermissionService.java
@@ -1,14 +1,17 @@
 package com.hzhg.plm.service;
 
 import com.hzhg.plm.entity.Permission;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Sql(scripts = {
@@ -24,46 +27,69 @@ public class TestPermissionService {
     @Autowired
     PermissionService permissionService;
 
+    @Autowired
+    UserService userService;
+
     @Test
     public void testGetPermissionsByRoleId() {
-        Set<String> permissionsExpect = new HashSet<>(
-                Arrays.asList("User:SELECT", "User:CREATE", "User:UPDATE", "User:DELETE"));
-        Set<Permission> permissions = permissionService.getPermissionsByRoleId(1L);
-        Assertions.assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
+        Set<String> permissionsExpect = Set.of(
+                "User:SELECT",
+                "User:UPDATE",
+                "Department:SELECT",
+                "Menu:SELECT",
+                "Role:SELECT"
+        );
+        Set<Permission> permissions = permissionService.getPermissionsByRoleId(2L);
+        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
     }
 
     @Test
     public void testGetPermissionsByRoleIds() {
-        Set<String> permissionsExpect = new HashSet<>(
-                Arrays.asList("User:SELECT", "User:CREATE", "User:UPDATE", "User:DELETE"));
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Collections.singleton(1L));
-        Assertions.assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
+        Set<String> permissionsExpect = Set.of(
+                "User:SELECT",
+                "User:UPDATE",
+                "Department:SELECT",
+                "Menu:SELECT",
+                "Role:SELECT"
+        );
+        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
+        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
     }
 
     @Test
     public void testAddRolePermissionsByRoleId() {
-        permissionService.addRolePermissions(2L, new HashSet<>(Arrays.asList(1L, 2L, 3L, 4L)));
-        Set<String> permissionsExpect = new HashSet<>(
-                Arrays.asList("User:SELECT", "User:CREATE", "User:UPDATE", "User:DELETE"));
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Collections.singleton(2L));
-        Assertions.assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
+        permissionService.addRolePermissions(2L, Set.of(1L, 2L, 3L, 4L));
+        Set<String> permissionsExpect = Set.of(
+                "User:SELECT",
+                "User:CREATE",
+                "User:UPDATE",
+                "User:DELETE",
+                "Department:SELECT",
+                "Menu:SELECT",
+                "Role:SELECT"
+        );
+        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
+        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
     }
 
     @Test
     public void testRemoveRolePermissionsByRoleId() {
-        permissionService.removeRolePermissions(1L, new HashSet<>(Arrays.asList(3L, 4L)));
-        Set<String> permissionsExpect = new HashSet<>(
-                Arrays.asList("User:SELECT", "User:CREATE"));
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Collections.singleton(1L));
-        Assertions.assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
+        permissionService.removeRolePermissions(2L, new HashSet<>(Arrays.asList(2L, 3L, 4L)));
+        Set<String> permissionsExpect = Set.of(
+                "User:SELECT",
+                "Department:SELECT",
+                "Menu:SELECT",
+                "Role:SELECT"
+        );
+        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
+        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
     }
 
     @Test
     public void testReplaceRolePermissionsByRoleId() {
-        permissionService.replaceRolePermissions(1L, new HashSet<>(Arrays.asList(1L, 2L)));
-        Set<String> permissionsExpect = new HashSet<>(
-                Arrays.asList("User:SELECT", "User:CREATE"));
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Collections.singleton(1L));
-        Assertions.assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
+        permissionService.replaceRolePermissions(2L, Set.of(1L, 2L, 3L, 4L));
+        Set<String> permissionsExpect = Set.of("User:SELECT", "User:CREATE", "User:UPDATE", "User:DELETE");
+        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
+        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
     }
 }
