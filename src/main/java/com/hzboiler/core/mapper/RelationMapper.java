111c95fb57306fe1ab8866e8ee55faa72f9bcc75
gongshuiwen <shuiwen.gong@outlook.com>
2024-05-07 22:25:50 +0800

    opt: optimize RelationMapper

--- src/main/java/com/hzboiler/core/mapper/RelationMapper.java
+++ src/main/java/com/hzboiler/core/mapper/RelationMapper.java
@@ -18,8 +18,34 @@ public interface RelationMapper {
     Map<Class<?>, Class<?>> mapperClass1 = new HashMap<>();
     Map<Class<?>, Class<?>> mapperClass2 = new HashMap<>();
 
+    @Select("SELECT ${targetField} FROM ${table} WHERE ${sourceField} = #{sourceId}")
+    List<Long> _getTargetIdsById(String table, String sourceField, String targetField, Long sourceId);
+
+    default List<Long> getTargetIds(Class<?> sourceClass, Long sourceId) {
+        if (sourceClass == null) {
+            throw new RuntimeException("sourceClass must not be null!");
+        }
+
+        if (sourceId == null) {
+            throw new RuntimeException("sourceId must not be null!");
+        }
+
+        // Get mapper interface
+        Class<?> mapperInterface = this.getClass().getInterfaces()[0];
+
+        // Get table name
+        String table = mapperTables.get(mapperInterface);
+
+        // Get the source field and target field
+        Map<String, String> fieldInfo = getFieldInfo(mapperInterface, sourceClass);
+        String sourceField = fieldInfo.get("sourceField");
+        String targetField = fieldInfo.get("targetField");
+
+        return _getTargetIdsById(table, sourceField, targetField, sourceId);
+    }
+
     @Select("SELECT ${targetField} FROM ${table} WHERE ${sourceField} IN (${sourceIds})")
-    List<Long> _getTargetIds(String table, String sourceField, String targetField, String sourceIds);
+    List<Long> _getTargetIdsByIds(String table, String sourceField, String targetField, String sourceIds);
 
     default List<Long> getTargetIds(Class<?> sourceClass, List<Long> sourceIds) {
         if (sourceClass == null) {
@@ -42,7 +68,7 @@ public interface RelationMapper {
         String targetField = fieldInfo.get("targetField");
 
         String sourceIdsString = sourceIds.stream().map(String::valueOf).collect(Collectors.joining(","));
-        return _getTargetIds(table, sourceField, targetField, sourceIdsString);
+        return _getTargetIdsByIds(table, sourceField, targetField, sourceIdsString);
     }
 
     @Insert("INSERT INTO ${table} (${sourceField}, ${targetField}) VALUES ${values}")
@@ -149,7 +175,7 @@ public interface RelationMapper {
             map.put("sourceField", mapperField2.get(mapperInterface));
             map.put("targetField", mapperField1.get(mapperInterface));
         } else {
-            throw new RuntimeException("the sourceClass '" + sourceClass + "' is invalid for RelationMapper '" + mapperInterface + "'!");
+            throw new RuntimeException("The sourceClass '" + sourceClass + "' is invalid for RelationMapper '" + mapperInterface + "'!");
         }
         return map;
     }
