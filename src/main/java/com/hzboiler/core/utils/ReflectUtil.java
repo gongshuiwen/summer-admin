df5afcf68f1c7476ccc078bf8c6b847e98aefae1
gongshuiwen <shuiwen.gong@outlook.com>
2024-05-15 21:54:42 +0800

    feat: add getAllDeclaredFieldsWithAnnotation and getAllDeclaredFieldsWithType in ReflectUtil

--- src/main/java/com/hzboiler/core/utils/ReflectUtil.java
+++ src/main/java/com/hzboiler/core/utils/ReflectUtil.java
@@ -1,5 +1,6 @@
 package com.hzboiler.core.utils;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,10 +8,47 @@ import java.util.List;
 
 public class ReflectUtil {
 
+    /**
+     * Get all declared fields (include inherited) for class
+     * @param clazz the class
+     * @return fields array
+     */
     public static Field[] getAllDeclaredFields(Class<?> clazz) {
         List<Field> fieldList = new ArrayList<>();
         while (clazz != null){
-            fieldList.addAll(new ArrayList<>(Arrays.asList(clazz.getDeclaredFields())));
+            fieldList.addAll(List.of(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
+        return fieldList.toArray(new Field[0]);
+    }
+
+    /**
+     * Get all declared fields (include inherited) with given annotation for class
+     * @param clazz the class
+     * @return fields array
+     */
+    public static Field[] getAllDeclaredFieldsWithAnnotation(Class<?> clazz, Class<? extends Annotation> annotation) {
+        List<Field> fieldList = new ArrayList<>();
+        while (clazz != null) {
+            Arrays.stream(clazz.getDeclaredFields())
+                    .filter(field -> field.isAnnotationPresent(annotation))
+                    .forEach(fieldList::add);
+            clazz = clazz.getSuperclass();
+        }
+        return fieldList.toArray(new Field[0]);
+    }
+
+    /**
+     * Get all declared fields (include inherited) with given type for class
+     * @param clazz the class
+     * @return fields array
+     */
+    public static Field[] getAllDeclaredFieldsWithType(Class<?> clazz, Class<?> fieldType) {
+        List<Field> fieldList = new ArrayList<>();
+        while (clazz != null) {
+            Arrays.stream(clazz.getDeclaredFields())
+                    .filter(field -> field.getType() == fieldType)
+                    .forEach(fieldList::add);
             clazz = clazz.getSuperclass();
         }
         return fieldList.toArray(new Field[0]);
