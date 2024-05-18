60feab308ad6af2a9d13b33987b212d2b174424b
gongshuiwen <shuiwen.gong@outlook.com>
2024-05-18 15:28:53 +0800

    feat: add CacheConfig

---
+++ src/main/java/com/hzboiler/core/config/CacheConfig.java
@@ -0,0 +1,38 @@
+package com.hzboiler.core.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    /**
+     * Note that the Qualifier name {@code springSessionDefaultRedisSerializer}
+     * means using the same {@link RedisSerializer} bean with Spring Session.
+     */
+    @Bean
+    public RedisCacheManager redisCacheManager(
+            RedisConnectionFactory redisConnectionFactory,
+            @Autowired @Qualifier("springSessionDefaultRedisSerializer")
+            RedisSerializer<Object> valueSerializer) {
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer))
+                .entryTtl(Duration.ofDays(1))
+                .disableCachingNullValues()
+                .computePrefixWith(name -> name + ":");;
+        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
+}
