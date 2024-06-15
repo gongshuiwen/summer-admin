b5520eb9c9a52c830a16f1edad17384bdf5dc9bc
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-15 19:02:22 +0800

    [IMP] core: add OnDelete CASCADE annotation for parentId field of BaseTreeModel

--- erp-core/src/main/java/com/hzboiler/erp/core/model/BaseTreeModel.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/model/BaseTreeModel.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.hzboiler.erp.core.field.Many2One;
 import com.hzboiler.erp.core.field.One2Many;
 import com.hzboiler.erp.core.field.annotations.InverseField;
+import com.hzboiler.erp.core.field.annotations.OnDelete;
 import com.hzboiler.erp.core.validation.CreateValidationGroup;
 import com.hzboiler.erp.core.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -25,6 +26,7 @@ public abstract class BaseTreeModel<T extends BaseTreeModel<?>> extends BaseMode
     private String parentPath;
 
     @Schema(description = "父级ID")
+    @OnDelete(OnDelete.Type.CASCADE)
     private Many2One<T> parentId;
 
     @Schema(description = "子级列表")
