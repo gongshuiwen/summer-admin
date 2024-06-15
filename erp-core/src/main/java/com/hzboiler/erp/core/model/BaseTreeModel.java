b101122fe1ad380ce22fe2410f268ee4a5f45c6a
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-15 13:07:32 +0800

    [IMP] core: change field type and optimize buildTree method of BaseTreeModel

--- erp-core/src/main/java/com/hzboiler/erp/core/model/BaseTreeModel.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/model/BaseTreeModel.java
@@ -1,6 +1,9 @@
 package com.hzboiler.erp.core.model;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.hzboiler.erp.core.field.Many2One;
+import com.hzboiler.erp.core.field.One2Many;
+import com.hzboiler.erp.core.field.annotations.InverseField;
 import com.hzboiler.erp.core.validation.CreateValidationGroup;
 import com.hzboiler.erp.core.validation.UpdateValidationGroup;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,10 +11,7 @@ import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author gongshuiwen
@@ -20,16 +20,17 @@ import java.util.Map;
 @Setter
 public abstract class BaseTreeModel<T extends BaseTreeModel<?>> extends BaseModel {
 
-    @Schema(description = "父级ID")
-    private Long parentId;
-
     @Schema(description = "父级路径")
     @Null(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String parentPath;
 
+    @Schema(description = "父级ID")
+    private Many2One<T> parentId;
+
     @Schema(description = "子级列表")
     @TableField(exist = false)
-    private List<T> children;
+    @InverseField("parentId")
+    private One2Many<T> children;
 
     /**
      * build a flat record list to a hierarchical tree, return roots' list
@@ -40,13 +41,17 @@ public abstract class BaseTreeModel<T extends BaseTreeModel<?>> extends BaseMode
     public static <T extends BaseTreeModel<T>> List<T> buildTree(List<T> records) {
         List<T> roots = new ArrayList<>();
         Map<Long, T> recordsMap = new HashMap<>(records.size());
+        Map<Long, List<T>> childrenMap = new HashMap<>();
 
-        // Loop 1: Build records map using id as key
+        // Loop 1: build records map using id as key
         records.forEach(record -> recordsMap.put(record.getId(), record));
 
-        // Loop 2
+        // Loop 2: process hierarchy
         records.forEach(record -> {
-            final Long parentId = record.getParentId();
+            Many2One<T> parentIdField = record.getParentId();
+            Objects.requireNonNull(parentIdField, "The parentId of record must not be null!");
+
+            Long parentId = parentIdField.getId();
             if (parentId == null || parentId == 0) {
                 // Add root record to roots' list
                 roots.add(record);
@@ -57,12 +62,16 @@ public abstract class BaseTreeModel<T extends BaseTreeModel<?>> extends BaseMode
                     throw new IllegalArgumentException("Parent record with id '" + parentId + "' doesn't exist!");
                 }
 
-                List<T> children = parent.getChildren();
-                if (children == null) {
-                    children = new ArrayList<>();
-                    parent.setChildren(children);
-                }
+                List<T> children = childrenMap.computeIfAbsent(parentId, k -> new ArrayList<>());
                 children.add(record);
+            }
+        });
+
+        // Loop 3: fill children field
+        records.forEach(record -> {
+            List<T> children = childrenMap.get(record.getId());
+            if (children != null) {
+                record.setChildren(One2Many.ofRecords(children));
             }
         });
 
