b3a35d54371ade3f1545c43e66d9e4069e670aae
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-15 21:43:29 +0800

    [IMP] base: change the MenuServiceImpl's superclass and optimize getMenusTree method

--- erp-module-base/src/main/java/com/hzboiler/erp/module/base/service/impl/MenuServiceImpl.java
+++ erp-module-base/src/main/java/com/hzboiler/erp/module/base/service/impl/MenuServiceImpl.java
@@ -1,8 +1,7 @@
 package com.hzboiler.erp.module.base.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.hzboiler.erp.core.service.AbstractBaseTreeService;
 import com.hzboiler.erp.module.base.model.Menu;
-import com.hzboiler.erp.core.service.AbstractBaseService;
 import com.hzboiler.erp.module.base.mapper.MenuMapper;
 import com.hzboiler.erp.module.base.service.MenuService;
 import lombok.extern.slf4j.Slf4j;
@@ -10,15 +9,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static com.hzboiler.erp.core.model.BaseTreeModel.buildTree;
-
 @Slf4j
 @Service
-public class MenuServiceImpl extends AbstractBaseService<MenuMapper, Menu> implements MenuService {
+public class MenuServiceImpl extends AbstractBaseTreeService<MenuMapper, Menu> implements MenuService {
 
     public List<Menu> getMenusTree() {
-        LambdaQueryWrapper<Menu> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.orderByAsc(Menu::getOrderNum);
-        return buildTree(list(queryWrapper));
+        return buildTree(lambdaQuery().orderByAsc(Menu::getOrderNum).list());
     }
 }
