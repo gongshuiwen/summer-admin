8a1c903aa1a6307b2d47b4c07afca2e7796ef209
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-15 21:43:57 +0800

    [IMP] base: add BaseUserService interface in UserService

--- erp-module-base/src/main/java/com/hzboiler/erp/module/base/service/UserService.java
+++ erp-module-base/src/main/java/com/hzboiler/erp/module/base/service/UserService.java
@@ -1,11 +1,12 @@
 package com.hzboiler.erp.module.base.service;
 
+import com.hzboiler.erp.core.service.BaseUserService;
 import com.hzboiler.erp.module.base.model.User;
 import com.hzboiler.erp.core.service.BaseService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserService extends BaseService<User>, UserDetailsService {
+public interface UserService extends BaseService<User>, UserDetailsService, BaseUserService {
 
     @Override
     User loadUserByUsername(String username) throws UsernameNotFoundException;
