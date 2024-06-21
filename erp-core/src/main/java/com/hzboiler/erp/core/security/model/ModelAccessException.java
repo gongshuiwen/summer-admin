09387d8d807d7db39accfb6e1f3525f4848a8250
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-21 22:09:44 +0800

    [IMP] core: optimize ModelAccessException

--- erp-core/src/main/java/com/hzboiler/erp/core/security/model/ModelAccessException.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/security/model/ModelAccessException.java
@@ -1,12 +1,15 @@
 package com.hzboiler.erp.core.security.model;
 
+import com.hzboiler.erp.core.exception.BusinessException;
+import com.hzboiler.erp.core.exception.CoreBusinessExceptionEnums;
 import com.hzboiler.erp.core.model.BaseModel;
 
 /**
  * @author gongshuiwen
  */
-public final class ModelAccessException extends RuntimeException {
+public final class ModelAccessException extends BusinessException {
     public ModelAccessException(Class<? extends BaseModel> modelClass, ModelAccessType authority) {
-        super("Model access denied: [" + authority.name() + "] " + modelClass.getSimpleName());
+        super(CoreBusinessExceptionEnums.ERROR_ACCESS_DENIED,
+                "Model access denied: [" + authority.name() + "] " + modelClass.getSimpleName());
     }
 }
