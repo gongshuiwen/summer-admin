fad4996bdda85022ad43573148b672eccc73ef4d
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-22 22:33:57 +0800

    [REF] core: refactor Query class

--- erp-core/src/main/java/com/hzboiler/erp/core/protocal/query/Query.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/protocal/query/Query.java
@@ -3,53 +3,41 @@ package com.hzboiler.erp.core.protocal.query;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hzboiler.erp.core.model.BaseModel;
+import lombok.Getter;
 
-public record Query<T extends BaseModel>(
-        @JsonIgnore Class<T> modelClass,
-        Long pageNum,
-        Long pageSize,
-        String sorts,
-        Condition condition
-) {
+/**
+ * @author gongshuiwen
+ */
+@Getter
+public class Query implements QueryWrapperAdapter {
 
-    public IPage<T> getPage() {
+    private final Long pageNum;
+    private final Long pageSize;
+    private final Condition condition;
+    private final OrderBys orderBys;
+
+    // prevent external instantiation
+    private Query(Long pageNum, Long pageSize, Condition condition, OrderBys orderBys) {
+        this.pageNum = pageNum;
+        this.pageSize = pageSize;
+        this.condition = condition;
+        this.orderBys = orderBys;
+    }
+
+    public static Query of(Long pageNum, Long pageSize, Condition condition, OrderBys orderBys) {
+        return new Query(pageNum, pageSize, condition, orderBys);
+    }
+
+    public <T extends BaseModel> IPage<T> getPage() {
         return new Page<>(pageNum, pageSize);
     }
 
-    public QueryWrapper<T> getQueryWrapper() {
-        QueryWrapper<T> queryWrapper = new QueryWrapper<>();
-        buildCondition(queryWrapper);
-        buildSorts(queryWrapper);
-        return queryWrapper;
-    }
-
-    private void buildCondition(QueryWrapper<T> queryWrapper) {
-        if (condition == null) {
-            return;
-        }
-        condition.applyToQueryWrapper(queryWrapper);
-    }
-
-    private void buildSorts(QueryWrapper<T> queryWrapper) {
-        if (sorts == null) {
-            return;
-        }
-
-        for (String s : sorts.split(",")) {
-            if (s.startsWith("_")) {
-                String fieldName = s.substring(1);
-                checkField(fieldName);
-                queryWrapper.orderByDesc(fieldName);
-            } else {
-                checkField(s);
-                queryWrapper.orderByAsc(s);
-            }
-        }
-    }
-
-    private void checkField(String fieldName) {
-        // TODO: implement check field existing in model class with cache
+    @Override
+    public void applyToQueryWrapper(QueryWrapper<? extends BaseModel> queryWrapper) {
+        if (condition != null)
+            condition.applyToQueryWrapper(queryWrapper);
+        if (orderBys != null)
+            orderBys.applyToQueryWrapper(queryWrapper);
     }
 }
