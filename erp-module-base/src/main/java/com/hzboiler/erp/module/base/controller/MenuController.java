408eb45531486d2c0f0b46af65ffdcbb2e24008f
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-24 22:59:42 +0800

    [DEPRECATE] base: remove tree api in MenuController

--- erp-module-base/src/main/java/com/hzboiler/erp/module/base/controller/MenuController.java
+++ erp-module-base/src/main/java/com/hzboiler/erp/module/base/controller/MenuController.java
@@ -1,25 +1,14 @@
 package com.hzboiler.erp.module.base.controller;
 
-import com.hzboiler.erp.module.base.model.Menu;
 import com.hzboiler.erp.core.controller.BaseController;
-import com.hzboiler.erp.core.protocal.Result;
+import com.hzboiler.erp.module.base.model.Menu;
 import com.hzboiler.erp.module.base.service.MenuService;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @Tag(name = "菜单接口")
 @RequestMapping("/menu")
 public class MenuController extends BaseController<MenuService, Menu> {
-
-    @Operation(summary = "查询菜单树")
-    @GetMapping("/tree")
-    public Result<List<Menu>> tree() {
-        return Result.success(service.getMenusTree());
-    }
 }
