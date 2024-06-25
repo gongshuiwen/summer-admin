4c934314af58800b33144ec51ec3b63cd4a11e73
gongshuiwen <shuiwen.gong@outlook.com>
2024-06-25 22:50:33 +0800

    [TEST] base: update tests for UserService

--- erp-module-base/src/test/java/com/hzboiler/erp/module/base/service/TestUserService.java
+++ erp-module-base/src/test/java/com/hzboiler/erp/module/base/service/TestUserService.java
@@ -1,15 +1,17 @@
 package com.hzboiler.erp.module.base.service;
 
-import com.hzboiler.erp.module.base.annotation.WithMockAdmin;
-import com.hzboiler.erp.module.base.model.Permission;
+import com.hzboiler.erp.core.context.BaseContextHolder;
+import com.hzboiler.erp.core.exception.BusinessException;
 import com.hzboiler.erp.module.base.model.Role;
 import com.hzboiler.erp.module.base.model.User;
+import com.hzboiler.erp.test.annotation.WithMockAdmin;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.HashSet;
@@ -17,20 +19,20 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.hzboiler.erp.core.security.Constants.CODE_BASE_USER;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * @author gongshuiwen
+ */
 @SpringBootTest
 @Sql(scripts = {
         "/sql/ddl/user.sql",
         "/sql/ddl/role.sql",
-        "/sql/ddl/permission.sql",
         "/sql/ddl/user_role.sql",
-        "/sql/ddl/role_permission.sql",
         "/sql/test/data/user.sql",
         "/sql/test/data/role.sql",
-        "/sql/test/data/permission.sql",
         "/sql/test/data/user_role.sql",
-        "/sql/test/data/role_permission.sql",
 })
 class TestUserService {
 
@@ -38,28 +40,26 @@ class TestUserService {
     UserService userService;
 
     @Autowired
-    PermissionService permissionService;
+    RoleService roleService;
 
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @AfterEach
+    void afterEach() {
+        BaseContextHolder.clearContext();
+    }
+
     @Test
+    @WithMockAdmin
     void testLoadUserByUsername() {
-        User user = userService.loadUserByUsername("admin");
-        assertEquals("admin", user.getUsername());
+        User userAdmin = userService.loadUserByUsername("admin");
+        assertEquals("admin", userAdmin.getUsername());
 
-        // Test Roles
-        Set<String> roles = new HashSet<>(List.of("SYS_ADMIN", "BASE_USER"));
-        assertEquals(roles, user.getRoles().get().stream().map(Role::getCode).collect(Collectors.toSet()));
+        User userDemo = userService.loadUserByUsername("demo");
+        assertEquals("demo", userDemo.getUsername());
 
-        // Test Authorities
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_SYS_ADMIN"));
-        authorities.add(new SimpleGrantedAuthority("ROLE_BASE_USER"));
-        Set<Long> roleIds = user.getRoles().get().stream().map(Role::getId).collect(Collectors.toSet());
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(roleIds);
-        permissions.forEach(perm -> authorities.add(new SimpleGrantedAuthority(perm.getCode())));
-        assertEquals(authorities, user.getAuthorities());
+        assertThrows(UsernameNotFoundException.class, () -> userService.loadUserByUsername("xxx"));
     }
 
     @Test
@@ -72,41 +72,63 @@ class TestUserService {
         userCreate.setPassword("123456");
         userService.createOne(userCreate);
 
+        // Test user
         User user = userService.loadUserByUsername("test");
         assertNotNull(user.getId());
 
-        // Test Password
+        // Test password
         assertTrue(passwordEncoder.matches("123456", user.getPassword()));
 
-        // Test Roles
-        Set<String> roles = new HashSet<>(List.of("BASE_USER"));
-        assertEquals(roles, user.getRoles().get().stream().map(Role::getCode).collect(Collectors.toSet()));
+        // Test roles
+        assertRoles(user, "BASE_USER");
+    }
 
-        // Test Authorities
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_BASE_USER"));
-        Set<Long> roleIds = user.getRoles().get().stream().map(Role::getId).collect(Collectors.toSet());
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(roleIds);
-        permissions.forEach(perm -> authorities.add(new SimpleGrantedAuthority(perm.getCode())));
-        assertEquals(authorities, user.getAuthorities());
+    private void assertRoles(User user, String... roleCodes) {
+        Set<String> roles = new HashSet<>(List.of(roleCodes));
+        assertEquals(roles, roleService.getRolesByUserId(user.getId()).stream().map(Role::getCode).collect(Collectors.toSet()));
     }
 
     @Test
     @WithMockAdmin
-    void testChangePassword() {
-        // Create user
-        User userCreate = new User();
-        userCreate.setUsername("test");
-        userCreate.setPassword("123456");
-        userService.createOne(userCreate);
+    void testUpdatePasswordAdmin() {
+        // Get user
+        User userDemo = userService.loadUserByUsername("demo");
 
-        // Change Password
+        // Change password
         String newPassword = "12345678";
         User userUpdate = new User();
         userUpdate.setPassword(newPassword);
-        userService.updateById(userCreate.getId(), userUpdate);
+        userService.updateById(userDemo.getId(), userUpdate);
 
-        // Test Password
-        assertTrue(passwordEncoder.matches(newPassword, userService.selectById(userCreate.getId()).getPassword()));
+        // Test password
+        assertTrue(passwordEncoder.matches(newPassword, userService.selectById(userDemo.getId()).getPassword()));
+    }
+
+    @Test
+    @WithMockUser(username = "demo", authorities = {CODE_BASE_USER, "S:User", "U:User"})
+    void testUpdatePasswordDemo() {
+        String newPassword = "12345678";
+
+        // Get user
+        User userAdmin = userService.loadUserByUsername("admin");
+        User userDemo = userService.loadUserByUsername("demo");
+
+        // Change password of demo
+        User userUpdate1 = new User();
+        userUpdate1.setPassword(newPassword);
+        assertDoesNotThrow(() -> userService.updateById(userDemo.getId(), userUpdate1));
+        assertTrue(passwordEncoder.matches(newPassword, userService.selectById(userDemo.getId()).getPassword()));
+
+        // Change password of admin
+        assertThrows(BusinessException.class, () -> userService.updateById(userAdmin.getId(), userUpdate1));
+
+        // Change nickname of demo
+        User userUpdate2 = new User();
+        userUpdate2.setNickname("xxx");
+        assertDoesNotThrow(() -> userService.updateById(userDemo.getId(), userUpdate2));
+        assertEquals("xxx", userService.selectById(userDemo.getId()).getNickname());
+
+        // Change nickname of admin
+        assertThrows(BusinessException.class, () -> userService.updateById(userAdmin.getId(), userUpdate2));
     }
 }
