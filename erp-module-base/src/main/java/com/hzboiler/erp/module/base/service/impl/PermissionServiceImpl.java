62f208a7c6497bbdf1daf1fddbd922cb55cd5cad
gongshuiwen <shuiwen.gong@outlook.com>
2024-07-01 22:15:00 +0800

    [IMP] base: optimize methods of PermissionServiceImpl

--- erp-module-base/src/main/java/com/hzboiler/erp/module/base/service/impl/PermissionServiceImpl.java
+++ erp-module-base/src/main/java/com/hzboiler/erp/module/base/service/impl/PermissionServiceImpl.java
@@ -1,17 +1,18 @@
 package com.hzboiler.erp.module.base.service.impl;
 
+import com.hzboiler.erp.core.service.AbstractBaseService;
+import com.hzboiler.erp.module.base.mapper.PermissionMapper;
 import com.hzboiler.erp.module.base.mapper.RolePermissionMapper;
 import com.hzboiler.erp.module.base.model.Permission;
 import com.hzboiler.erp.module.base.model.Role;
-import com.hzboiler.erp.core.service.AbstractBaseService;
-import com.hzboiler.erp.module.base.mapper.PermissionMapper;
 import com.hzboiler.erp.module.base.service.PermissionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,61 +21,52 @@ import java.util.stream.Collectors;
 public class PermissionServiceImpl extends AbstractBaseService<PermissionMapper, Permission> implements PermissionService {
 
     @Autowired
-    PermissionMapper permissionMapper;
-
-    @Autowired
     RolePermissionMapper rolePermissionMapper;
 
     @Override
     @Transactional
     public Set<Permission> getPermissionsByRoleId(Long roleId) {
-        if (roleId == null) {
-            throw new IllegalArgumentException();
-        }
-        Set<Long> permIds = rolePermissionMapper.getPermissionIdsByRoleId(roleId);
-        return new HashSet<>(permissionMapper.selectBatchIds(permIds));
+        Objects.requireNonNull(roleId, "roleId must not be null");
+        List<Long> permIds = rolePermissionMapper.getTargetIds(Role.class, roleId);
+        return getBaseMapper().selectBatchIds(permIds).stream().collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
     @Transactional
     public Set<Permission> getPermissionsByRoleIds(Set<Long> roleIds) {
-        if (roleIds == null) {
-            throw new IllegalArgumentException();
-        }
-        Set<Long> permIds = rolePermissionMapper.getPermissionIdsByRoleIds(roleIds);
-        if (permIds.isEmpty()) {
-            return Set.of();
-        }
+        Objects.requireNonNull(roleIds, "roleIds must not be null");
+        if (roleIds.isEmpty()) throw new IllegalArgumentException("roleIds must not be empty");
 
-        return permissionMapper.selectBatchIds(permIds).stream().collect(Collectors.toUnmodifiableSet());
+        List<Long> permIds = rolePermissionMapper.getTargetIds(Role.class, roleIds.stream().toList());
+        if (permIds.isEmpty())
+            return Set.of();
+
+        return getBaseMapper().selectBatchIds(permIds).stream().collect(Collectors.toUnmodifiableSet());
     }
 
     @Override
     @Transactional
     public void addRolePermissions(Long roleId, Set<Long> permIds) {
-        if (roleId == null || permIds == null || permIds.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        Set<Long> existPermIds = rolePermissionMapper.getPermissionIdsByRoleId(roleId);
-        rolePermissionMapper.add(Role.class, roleId, permIds.stream().filter(id -> !existPermIds.contains(id)).toList());
+        Objects.requireNonNull(roleId, "roleId must not be null");
+        Objects.requireNonNull(permIds, "permIds must not be null");
+        if (permIds.isEmpty()) throw new IllegalArgumentException("permIds must not be empty");
+        rolePermissionMapper.add(Role.class, roleId, permIds.stream().toList());
     }
 
     @Override
     @Transactional
     public void removeRolePermissions(Long roleId, Set<Long> permIds) {
-        if (roleId == null || permIds == null || permIds.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        rolePermissionMapper.removeRolePermissions(roleId, permIds);
+        Objects.requireNonNull(roleId, "roleId must not be null");
+        Objects.requireNonNull(permIds, "permIds must not be null");
+        if (permIds.isEmpty()) throw new IllegalArgumentException("permIds must not be empty");
+        rolePermissionMapper.remove(Role.class, roleId, permIds.stream().toList());
     }
 
     @Override
     @Transactional
     public void replaceRolePermissions(Long roleId, Set<Long> permIds) {
-        if (roleId == null || permIds == null || permIds.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        rolePermissionMapper.removeRolePermissionsAll(roleId);
-        rolePermissionMapper.addRolePermissions(roleId, permIds);
+        Objects.requireNonNull(roleId, "roleId must not be null");
+        Objects.requireNonNull(permIds, "permIds must not be null");
+        rolePermissionMapper.replace(Role.class, roleId, permIds.stream().toList());
     }
 }
