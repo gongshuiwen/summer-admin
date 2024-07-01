c3801a6b0a3321d524a14939437e2424c80f3194
gongshuiwen <shuiwen.gong@outlook.com>
2024-07-01 22:15:15 +0800

    [TEST] base: update tests for PermissionService

--- erp-module-base/src/test/java/com/hzboiler/erp/module/base/service/TestPermissionService.java
+++ erp-module-base/src/test/java/com/hzboiler/erp/module/base/service/TestPermissionService.java
@@ -1,13 +1,12 @@
 package com.hzboiler.erp.module.base.service;
 
 import com.hzboiler.erp.module.base.model.Permission;
+import com.hzboiler.erp.module.base.model.Role;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -33,11 +32,11 @@ class TestPermissionService {
     @Test
     void testGetPermissionsByRoleId() {
         Set<String> permissionsExpect = Set.of(
-                "User:SELECT",
-                "User:UPDATE",
-                "Department:SELECT",
-                "Menu:SELECT",
-                "Role:SELECT"
+                "S:User",
+                "U:User",
+                "S:Department",
+                "S:Menu",
+                "S:Role"
         );
         Set<Permission> permissions = permissionService.getPermissionsByRoleId(2L);
         assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
@@ -46,49 +45,12 @@ class TestPermissionService {
     @Test
     void testGetPermissionsByRoleIds() {
         Set<String> permissionsExpect = Set.of(
-                "User:SELECT",
-                "User:UPDATE",
-                "Department:SELECT",
-                "Menu:SELECT",
-                "Role:SELECT"
+                "S:User",
+                "U:User",
+                "S:Department",
+                "S:Menu",
+                "S:Role"
         );
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
-        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
-    }
-
-    @Test
-    void testAddRolePermissionsByRoleId() {
-        permissionService.addRolePermissions(2L, Set.of(1L, 2L, 3L, 4L));
-        Set<String> permissionsExpect = Set.of(
-                "User:SELECT",
-                "User:CREATE",
-                "User:UPDATE",
-                "User:DELETE",
-                "Department:SELECT",
-                "Menu:SELECT",
-                "Role:SELECT"
-        );
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
-        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
-    }
-
-    @Test
-    void testRemoveRolePermissionsByRoleId() {
-        permissionService.removeRolePermissions(2L, new HashSet<>(Arrays.asList(2L, 3L, 4L)));
-        Set<String> permissionsExpect = Set.of(
-                "User:SELECT",
-                "Department:SELECT",
-                "Menu:SELECT",
-                "Role:SELECT"
-        );
-        Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
-        assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
-    }
-
-    @Test
-    void testReplaceRolePermissionsByRoleId() {
-        permissionService.replaceRolePermissions(2L, Set.of(1L, 2L, 3L, 4L));
-        Set<String> permissionsExpect = Set.of("User:SELECT", "User:CREATE", "User:UPDATE", "User:DELETE");
         Set<Permission> permissions = permissionService.getPermissionsByRoleIds(Set.of(2L));
         assertEquals(permissionsExpect, permissions.stream().map(Permission::getCode).collect(Collectors.toSet()));
     }
