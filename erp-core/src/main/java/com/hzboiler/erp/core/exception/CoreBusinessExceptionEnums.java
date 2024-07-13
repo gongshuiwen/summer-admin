c8b8c1ff89169c59aa475ad4dd8ba09bcabdd01f
gongshuiwen <shuiwen.gong@outlook.com>
2024-07-13 11:38:19 +0800

    [IMP] core: optimize CoreBusinessExceptionEnums

--- erp-core/src/main/java/com/hzboiler/erp/core/exception/CoreBusinessExceptionEnums.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/exception/CoreBusinessExceptionEnums.java
@@ -1,7 +1,7 @@
 package com.hzboiler.erp.core.exception;
 
 /**
- * Static utility class to define the {@link BusinessExceptionEnum} constants of namespace 'core'.
+ * Static class to define the {@link BusinessExceptionEnum} constants of namespace {@code core}.
  *
  * @author gongshuiwen
  * @see BusinessExceptionEnum
@@ -10,16 +10,21 @@ public final class CoreBusinessExceptionEnums {
 
     public static final String CORE_NAME_SPACE = "core";
 
+    // authentication and authorization
     public static final BusinessExceptionEnum ERROR_AUTHENTICATION_FAILED = new BusinessExceptionEnum(CORE_NAME_SPACE, 80001, "身份认证失败！");
     public static final BusinessExceptionEnum ERROR_ACCESS_DENIED = new BusinessExceptionEnum(CORE_NAME_SPACE, 80002, "权限验证失败，无当前资源的访问权限！");
 
-    public static final BusinessExceptionEnum ERROR_TEST = new BusinessExceptionEnum(CORE_NAME_SPACE, 90000, "This error is for test!");
+    // api related
     public static final BusinessExceptionEnum ERROR_API_NOT_IMPLEMENTED = new BusinessExceptionEnum(CORE_NAME_SPACE, 90001, "API is not implemented!");
     public static final BusinessExceptionEnum ERROR_API_DEPRECATED = new BusinessExceptionEnum(CORE_NAME_SPACE, 90002, "API is deprecated!");
     public static final BusinessExceptionEnum ERROR_INVALID_ARGUMENTS = new BusinessExceptionEnum(CORE_NAME_SPACE, 90003, "Invalid arguments!");
     public static final BusinessExceptionEnum ERROR_INVALID_REQUEST_BODY = new BusinessExceptionEnum(CORE_NAME_SPACE, 90004, "Invalid request body!");
+
+    // validation
     public static final BusinessExceptionEnum ERROR_VALIDATION_FAILED = new BusinessExceptionEnum(CORE_NAME_SPACE, 90005, "Validation failed!");
 
+    // other
+    public static final BusinessExceptionEnum ERROR_TEST = new BusinessExceptionEnum(CORE_NAME_SPACE, 99990, "This error is for test!");
     public static final BusinessExceptionEnum ERROR_INTERNAL = new BusinessExceptionEnum(CORE_NAME_SPACE, 99999, "Server internal error!");
 
     // prevent external instantiation
