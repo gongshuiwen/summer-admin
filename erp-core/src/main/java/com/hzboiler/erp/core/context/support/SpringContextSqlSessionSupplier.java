04cae2a7d3ea5db8a5c3a08cb9f2b5783b184873
gongshuiwen <shuiwen.gong@outlook.com>
2024-08-02 22:06:53 +0800

    [IMP] core: optimize SpringContextSqlSessionSupplier implement

--- erp-core/src/main/java/com/hzboiler/erp/core/context/support/SpringContextSqlSessionSupplier.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/context/support/SpringContextSqlSessionSupplier.java
@@ -2,7 +2,6 @@ package com.hzboiler.erp.core.context.support;
 
 import com.hzboiler.erp.core.util.SpringContextUtil;
 import org.apache.ibatis.session.SqlSession;
-import org.mybatis.spring.SqlSessionTemplate;
 
 /**
  * @author gongshuiwen
@@ -11,6 +10,6 @@ public class SpringContextSqlSessionSupplier implements SqlSessionSupplier {
 
     @Override
     public SqlSession get() {
-        return SpringContextUtil.getBean(SqlSessionTemplate.class);
+        return SpringContextUtil.getBean(SqlSession.class);
     }
 }
