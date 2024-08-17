9b769812c0767d9e5c6a60d8c680ae6c3f270604
gongshuiwen <gongshuiwem@gmail.com>
2024-08-17 20:56:05 +0800

    [TEST] core: move TestSimpleGrantedAuthorityPool from package com.hzboiler.erp.core.security to com.hzboiler.erp.core.security.authorization

--- erp-core/src/test/java/com/hzboiler/erp/core/security/authorization/TestSimpleGrantedAuthorityPool.java
+++ erp-core/src/test/java/com/hzboiler/erp/core/security/authorization/TestSimpleGrantedAuthorityPool.java
@@ -1,6 +1,5 @@
-package com.hzboiler.erp.core.security;
+package com.hzboiler.erp.core.security.authorization;
 
-import com.hzboiler.erp.core.security.authorization.SimpleGrantedAuthorityPool;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotSame;
