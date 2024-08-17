12fb82399beb43bdeae02238382622fa43b7b25d
gongshuiwen <gongshuiwem@gmail.com>
2024-08-17 16:49:23 +0800

    [IMP] core: add @NonNull annotations for afterCompletion method of BaseInterceptor

--- erp-core/src/main/java/com/hzboiler/erp/core/web/BaseInterceptor.java
+++ erp-core/src/main/java/com/hzboiler/erp/core/web/BaseInterceptor.java
@@ -3,6 +3,7 @@ package com.hzboiler.erp.core.web;
 import com.hzboiler.erp.core.context.BaseContextHolder;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
@@ -11,7 +12,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public final class BaseInterceptor implements HandlerInterceptor {
 
     @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+    public void afterCompletion(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+                                @NonNull Object handler, Exception ex) {
         BaseContextHolder.clearContext();
     }
 }
