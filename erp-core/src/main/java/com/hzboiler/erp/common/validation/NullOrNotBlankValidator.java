e1d1873b737118f3464d1db1ae74c65e34128358
gongshuiwen <gongshuiwem@gmail.com>
2024-08-17 20:12:29 +0800

    [IMP] core: remove public and add final flag for NullOrNotBlankValidator

--- erp-core/src/main/java/com/hzboiler/erp/common/validation/NullOrNotBlankValidator.java
+++ erp-core/src/main/java/com/hzboiler/erp/common/validation/NullOrNotBlankValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidatorContext;
 /**
  * @author gongshuiwen
  */
-public class NullOrNotBlankValidator implements ConstraintValidator<NullOrNotBlank, String> {
+final class NullOrNotBlankValidator implements ConstraintValidator<NullOrNotBlank, String> {
 
     public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
         return value == null || !value.isBlank();
