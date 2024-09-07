38bb36a202eb57574c4cc5b7ad521ff168850341
gongshuiwen <gongshuiwen@gmail.com>
2024-09-07 16:51:57 +0800

    [REFACTOR] project: rename module admin-server to server and the package of Application class of server module

--- server/src/main/java/io/summernova/admin/Application.java
+++ server/src/main/java/io/summernova/admin/Application.java
@@ -1,4 +1,4 @@
-package io.summernova.admin.server;
+package io.summernova.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
