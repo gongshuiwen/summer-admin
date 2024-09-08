616b44782a5bbb3b2ebc1a8ba09e748dfd836226
gongshuiwen <gongshuiwen@gmail.com>
2024-09-08 09:58:28 +0800

    [DOC] server: add doc string

--- server/src/main/java/io/summernova/admin/Application.java
+++ server/src/main/java/io/summernova/admin/Application.java
@@ -3,6 +3,9 @@ package io.summernova.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * author gongshuiwen
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
