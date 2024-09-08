90d98c46f3532143f359c9dfdf233ae904c37393
gongshuiwen <gongshuiwen@gmail.com>
2024-09-08 12:50:47 +0800

    [IMP] module-base: add module-info.java

---
+++ module-base/src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module summernova.admin.module.base {
+    requires summernova.admin.core;
+
+    requires com.fasterxml.jackson.annotation;
+
+    requires org.mybatis;
+    requires com.baomidou.mybatis.plus.annotation;
+    requires com.baomidou.mybatis.plus.core;
+
+    requires spring.beans;
+    requires spring.context;
+    requires spring.tx;
+
+    requires spring.security.core;
+    requires spring.security.crypto;
+
+    requires io.swagger.v3.oas.annotations;
+
+    exports io.summernova.admin.module.base.model;
+    exports io.summernova.admin.module.base.service;
+
+    opens io.summernova.admin.module.base.model to com.baomidou.mybatis.plus.core, summernova.admin.core;
+    opens io.summernova.admin.module.base.service to spring.core;
+    opens io.summernova.admin.module.base.sql.ddl;
+}
