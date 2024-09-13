99695c09fdc578e83d40ed550b9541f27fd86179
gongshuiwen <gongshuiwen@gmail.com>
2024-09-13 22:38:02 +0800

    [TEST] admin-common: update tests for CompositeCondition and SimpleCondition

--- admin-core/src/test/java/io/summernova/admin/core/protocal/query/TestCompositeCondition.java
+++ admin-core/src/test/java/io/summernova/admin/core/protocal/query/TestCompositeCondition.java
@@ -1,11 +1,7 @@
 package io.summernova.admin.core.protocal.query;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import io.summernova.admin.core.model.Mock;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,125 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author gongshuiwen
  */
 class TestCompositeCondition {
-
-    static final LocalDateTime CREATE_TIME = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
-
-    static final List<Condition> SIMPLE_QUERY_CONDITIONS = Arrays.asList(
-            SimpleCondition.of("name", "=", "mock"),
-            SimpleCondition.of("create_time", ">=", CREATE_TIME),
-            SimpleCondition.of("create_user", "=", 1)
-    );
-
-    @Test
-    void testApplyToQueryWrapperCompositeAnd() {
-        QueryWrapper<Mock> mockQueryWrapper = new QueryWrapper<>();
-        Condition condition = CompositeCondition.and(SIMPLE_QUERY_CONDITIONS);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(" +
-                "(name = #{ew.paramNameValuePairs.MPGENVAL1}) " +
-                "AND " +
-                "(create_time >= #{ew.paramNameValuePairs.MPGENVAL2}) " +
-                "AND " +
-                "(create_user = #{ew.paramNameValuePairs.MPGENVAL3}))", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-        assertEquals(CREATE_TIME, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL2"));
-        assertEquals(1, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL3"));
-    }
-
-    @Test
-    void testApplyToQueryWrapperCompositeOr() {
-        QueryWrapper<Mock> mockQueryWrapper = new QueryWrapper<>();
-        Condition condition = CompositeCondition.or(SIMPLE_QUERY_CONDITIONS);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(" +
-                "(name = #{ew.paramNameValuePairs.MPGENVAL1}) " +
-                "OR " +
-                "(create_time >= #{ew.paramNameValuePairs.MPGENVAL2}) " +
-                "OR " +
-                "(create_user = #{ew.paramNameValuePairs.MPGENVAL3}))", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-        assertEquals(CREATE_TIME, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL2"));
-        assertEquals(1, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL3"));
-    }
-
-    @Test
-    void testApplyToQueryWrapperCompositeNot() {
-        QueryWrapper<Mock> mockQueryWrapper = new QueryWrapper<>();
-        Condition condition = CompositeCondition.not(SimpleCondition.of("name", "=", "mock"));
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(NOT (name = #{ew.paramNameValuePairs.MPGENVAL1}))", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-    }
-
-    @Test
-    void testApplyToQueryWrapperCompositeNotAnd() {
-        QueryWrapper<Mock> mockQueryWrapper = new QueryWrapper<>();
-        Condition condition = CompositeCondition.notAnd(SIMPLE_QUERY_CONDITIONS);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(" +
-                "NOT (" +
-                "(name = #{ew.paramNameValuePairs.MPGENVAL1}) " +
-                "AND " +
-                "(create_time >= #{ew.paramNameValuePairs.MPGENVAL2}) " +
-                "AND " +
-                "(create_user = #{ew.paramNameValuePairs.MPGENVAL3})))", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-        assertEquals(CREATE_TIME, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL2"));
-        assertEquals(1, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL3"));
-    }
-
-    @Test
-    void testApplyToQueryWrapperCompositeNotOr() {
-        QueryWrapper<Mock> mockQueryWrapper = new QueryWrapper<>();
-        Condition condition = CompositeCondition.notOr(SIMPLE_QUERY_CONDITIONS);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(" +
-                "NOT (" +
-                "(name = #{ew.paramNameValuePairs.MPGENVAL1}) " +
-                "OR " +
-                "(create_time >= #{ew.paramNameValuePairs.MPGENVAL2}) " +
-                "OR " +
-                "(create_user = #{ew.paramNameValuePairs.MPGENVAL3})))", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-        assertEquals(CREATE_TIME, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL2"));
-        assertEquals(1, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL3"));
-    }
-
-    @Test
-    void testApplyToQueryWrapperCompositeNested() {
-        CompositeCondition subCondition1 = CompositeCondition.or(List.of(
-                SimpleCondition.of("name", "=", "mock1"),
-                SimpleCondition.of("name", "=", "mock2")
-        ));
-
-        CompositeCondition subCondition2 = CompositeCondition.and(List.of(
-                SimpleCondition.of("create_time", ">", LocalDateTime.of(2020, 1, 1, 0, 0, 0)),
-                SimpleCondition.of("create_time", "<", LocalDateTime.of(2022, 1, 1, 0, 0, 0))
-        ));
-
-        SimpleCondition subCondition3 = SimpleCondition.of("create_user", "!=", 1);
-
-        Condition condition = CompositeCondition.and(List.of(subCondition1, subCondition2, subCondition3));
-
-        QueryWrapper<Mock> mockQueryWrapper = new QueryWrapper<>();
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(" +
-                "(" +
-                "(name = #{ew.paramNameValuePairs.MPGENVAL1}) " +
-                "OR " +
-                "(name = #{ew.paramNameValuePairs.MPGENVAL2})" +
-                ") " +
-                "AND " +
-                "(" +
-                "(create_time > #{ew.paramNameValuePairs.MPGENVAL3}) " +
-                "AND " +
-                "(create_time < #{ew.paramNameValuePairs.MPGENVAL4})" +
-                ") " +
-                "AND " +
-                "(create_user <> #{ew.paramNameValuePairs.MPGENVAL5}))", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock1", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-        assertEquals("mock2", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL2"));
-    }
 
     @Test
     void testGetSql() {
--- admin-core/src/test/java/io/summernova/admin/core/protocal/query/TestSimpleCondition.java
+++ admin-core/src/test/java/io/summernova/admin/core/protocal/query/TestSimpleCondition.java
@@ -1,10 +1,6 @@
 package io.summernova.admin.core.protocal.query;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import io.summernova.admin.core.model.Mock;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,92 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author gongshuiwen
  */
 class TestSimpleCondition {
-
-    static final LocalDateTime CREATE_TIME = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
-
-    @Test
-    void testApplyToQueryWrapperGeneral() {
-        QueryWrapper<Mock> mockQueryWrapper;
-        Condition condition;
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("id", "=", 100);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(id = #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals(100, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("id", "<", 100);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(id < #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals(100, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("id", ">", 100);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(id > #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals(100, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("id", "!=", 100);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(id <> #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals(100, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("id", "<=", 100);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(id <= #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals(100, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("id", ">=", 100);
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(id >= #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals(100, mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-    }
-
-    @Test
-    void testApplyToQueryWrapperLike() {
-        QueryWrapper<Mock> mockQueryWrapper;
-        Condition condition;
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("name", "like", "mock");
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(name LIKE #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals("%mock%", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("name", "likeLeft", "mock");
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(name LIKE #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals("%mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("name", "likeRight", "mock");
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(name LIKE #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock%", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("name", "notLike", "mock");
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(name NOT LIKE #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals("%mock%", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("name", "notLikeLeft", "mock");
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(name NOT LIKE #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals("%mock", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-
-        mockQueryWrapper = new QueryWrapper<>();
-        condition = SimpleCondition.of("name", "notLikeRight", "mock");
-        condition.applyToQueryWrapper(mockQueryWrapper);
-        assertEquals("(name NOT LIKE #{ew.paramNameValuePairs.MPGENVAL1})", mockQueryWrapper.getSqlSegment());
-        assertEquals("mock%", mockQueryWrapper.getParamNameValuePairs().get("MPGENVAL1"));
-    }
 
     @Test
     void testGetSql() {
