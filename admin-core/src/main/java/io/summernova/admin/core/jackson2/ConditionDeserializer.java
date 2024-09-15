8918c92a657905f6c7d6c4825a0f890f63dde76b
gongshuiwen <gongshuiwen@gmail.com>
2024-09-15 18:45:58 +0800

    [IMP] admin-core: optimize ConditionDeserializer to support more condition and update tests

--- admin-core/src/main/java/io/summernova/admin/core/jackson2/ConditionDeserializer.java
+++ admin-core/src/main/java/io/summernova/admin/core/jackson2/ConditionDeserializer.java
@@ -5,15 +5,21 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import io.summernova.admin.common.query.CompositeCondition;
-import io.summernova.admin.common.query.Condition;
-import io.summernova.admin.common.query.SimpleCondition;
+import io.summernova.admin.common.query.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConditionDeserializer extends StdDeserializer<Condition> {
+
+    private static final String FIELD = "field";
+    private static final String OPERATOR = "operator";
+    private static final String VALUE = "value";
+    private static final String VALUE1 = "value1";
+    private static final String VALUE2 = "value2";
+    private static final String VALUES = "values";
+    private static final String CONDITIONS = "conditions";
 
     protected ConditionDeserializer(Class<?> vc) {
         super(vc);
@@ -27,21 +33,53 @@ public class ConditionDeserializer extends StdDeserializer<Condition> {
     public Condition deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         ObjectMapper mapper = (ObjectMapper) jp.getCodec();
         JsonNode node = mapper.readTree(jp);
+        String operator = node.get(OPERATOR).asText();
+        if (operator == null)
+            throw new IllegalArgumentException("Invalid JSON structure for Condition, operator is missing.");
 
-        if (node.has("field")) {
-            String field = node.get("field").asText();
-            String operator = node.get("operator").asText();
-            String value = node.get("value").asText();
-            return SimpleCondition.of(field, operator, value);
-        } else if (node.has("conditions")) {
-            String operator = node.get("operator").asText();
+        if (GeneralOperator.contains(operator)) {
+            String field = node.get(FIELD).asText();
+            String value = node.get(VALUE).asText();
+            return GeneralCondition.of(field, operator, value);
+        } else if (LikeOperator.contains(operator)) {
+            String field = node.get(FIELD).asText();
+            String value = node.get(VALUE).asText();
+            return LikeCondition.of(field, operator, value);
+        } else if (BetweenCondition.OPERATOR_BETWEEN.equals(operator)) {
+            String field = node.get(FIELD).asText();
+            String value1 = node.get(VALUE1).asText();
+            String value2 = node.get(VALUE2).asText();
+            return BetweenCondition.between(field, value1, value2);
+        } else if (BetweenCondition.OPERATOR_NOT_BETWEEN.equals(operator)) {
+            String field = node.get(FIELD).asText();
+            String value1 = node.get(VALUE1).asText();
+            String value2 = node.get(VALUE2).asText();
+            return BetweenCondition.notBetween(field, value1, value2);
+        } else if (NullCondition.OPERATOR_IS_NULL.equals(operator)) {
+            String field = node.get(FIELD).asText();
+            return NullCondition.isNull(field);
+        } else if (NullCondition.OPERATOR_IS_NOT_NULL.equals(operator)) {
+            String field = node.get(FIELD).asText();
+            return NullCondition.isNotNull(field);
+        } else if (InCondition.OPERATOR_IN.equals(operator)) {
+            String field = node.get(FIELD).asText();
+            List<String> values = new ArrayList<>();
+            for (JsonNode valueNode : node.get(VALUES))
+                values.add(valueNode.asText());
+            return InCondition.in(field, values);
+        } else if (InCondition.OPERATOR_NOT_IN.equals(operator)) {
+            String field = node.get(FIELD).asText();
+            List<Object> values = new ArrayList<>();
+            for (JsonNode valueNode : node.get(VALUES))
+                values.add(valueNode.asText());
+            return InCondition.notIn(field, values);
+        } else if (CompositeOperator.contains(operator)) {
             List<Condition> conditions = new ArrayList<>();
-            for (JsonNode conditionNode : node.get("conditions")) {
+            for (JsonNode conditionNode : node.get(CONDITIONS))
                 conditions.add(mapper.treeToValue(conditionNode, Condition.class));
-            }
             return CompositeCondition.of(operator, conditions);
+        } else {
+            throw new IllegalArgumentException("Invalid JSON structure for Condition, invalid operator: " + operator);
         }
-
-        throw new IllegalArgumentException("Invalid JSON structure for Condition");
     }
 }
--- admin-core/src/test/java/io/summernova/admin/core/jackson2/TestConditionDeserializer.java
+++ admin-core/src/test/java/io/summernova/admin/core/jackson2/TestConditionDeserializer.java
@@ -3,12 +3,11 @@ package io.summernova.admin.core.jackson2;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import io.summernova.admin.common.query.CompositeCondition;
-import io.summernova.admin.common.query.Condition;
-import io.summernova.admin.common.query.SimpleCondition;
+import io.summernova.admin.common.query.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 /**
  * @author gongshuiwen
@@ -16,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestConditionDeserializer {
 
     ObjectMapper mapper;
+
     {
         mapper = new ObjectMapper();
         SimpleModule module = new SimpleModule();
@@ -24,40 +24,262 @@ class TestConditionDeserializer {
     }
 
     @Test
-    void testSimpleCondition() throws JsonProcessingException {
+    void testEqCondition() throws JsonProcessingException {
         Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"=\",\"value\":\"100\"}", Condition.class);
-        assertInstanceOf(SimpleCondition.class, condition);
-        SimpleCondition simpleCondition = (SimpleCondition) condition;
-        assertEquals("id", simpleCondition.getField());
-        assertEquals("=", simpleCondition.getOperator());
-        assertEquals("100", simpleCondition.getValue());
+        assertInstanceOf(GeneralCondition.class, condition);
+        GeneralCondition generalCondition = (GeneralCondition) condition;
+        assertEquals("id", generalCondition.getField());
+        assertEquals("=", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
     }
 
     @Test
-    void testCompositeCondition() throws JsonProcessingException {
+    void testNeCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"!=\",\"value\":\"100\"}", Condition.class);
+        assertInstanceOf(GeneralCondition.class, condition);
+        GeneralCondition generalCondition = (GeneralCondition) condition;
+        assertEquals("id", generalCondition.getField());
+        assertEquals("!=", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
+    }
+
+    @Test
+    void testLtCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"<\",\"value\":\"100\"}", Condition.class);
+        assertInstanceOf(GeneralCondition.class, condition);
+        GeneralCondition generalCondition = (GeneralCondition) condition;
+        assertEquals("id", generalCondition.getField());
+        assertEquals("<", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
+    }
+
+    @Test
+    void testGtCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\">\",\"value\":\"100\"}", Condition.class);
+        assertInstanceOf(GeneralCondition.class, condition);
+        GeneralCondition generalCondition = (GeneralCondition) condition;
+        assertEquals("id", generalCondition.getField());
+        assertEquals(">", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
+    }
+
+    @Test
+    void testLeCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"<=\",\"value\":\"100\"}", Condition.class);
+        assertInstanceOf(GeneralCondition.class, condition);
+        GeneralCondition generalCondition = (GeneralCondition) condition;
+        assertEquals("id", generalCondition.getField());
+        assertEquals("<=", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
+    }
+
+    @Test
+    void testGeCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\">=\",\"value\":\"100\"}", Condition.class);
+        assertInstanceOf(GeneralCondition.class, condition);
+        GeneralCondition generalCondition = (GeneralCondition) condition;
+        assertEquals("id", generalCondition.getField());
+        assertEquals(">=", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
+    }
+
+    @Test
+    void testLikeCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"name\",\"operator\":\"like\",\"value\":\"mock\"}", Condition.class);
+        assertInstanceOf(LikeCondition.class, condition);
+        LikeCondition likeCondition = (LikeCondition) condition;
+        assertEquals("name", likeCondition.getField());
+        assertEquals("like", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testLikeLeftCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"name\",\"operator\":\"likeLeft\",\"value\":\"mock\"}", Condition.class);
+        assertInstanceOf(LikeCondition.class, condition);
+        LikeCondition likeCondition = (LikeCondition) condition;
+        assertEquals("name", likeCondition.getField());
+        assertEquals("likeLeft", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testLikeRightCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"name\",\"operator\":\"likeRight\",\"value\":\"mock\"}", Condition.class);
+        assertInstanceOf(LikeCondition.class, condition);
+        LikeCondition likeCondition = (LikeCondition) condition;
+        assertEquals("name", likeCondition.getField());
+        assertEquals("likeRight", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testNotLikeCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"name\",\"operator\":\"notLike\",\"value\":\"mock\"}", Condition.class);
+        assertInstanceOf(LikeCondition.class, condition);
+        LikeCondition likeCondition = (LikeCondition) condition;
+        assertEquals("name", likeCondition.getField());
+        assertEquals("notLike", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testNotLikeLeftCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"name\",\"operator\":\"notLikeLeft\",\"value\":\"mock\"}", Condition.class);
+        assertInstanceOf(LikeCondition.class, condition);
+        LikeCondition likeCondition = (LikeCondition) condition;
+        assertEquals("name", likeCondition.getField());
+        assertEquals("notLikeLeft", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testNotLikeRightCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"name\",\"operator\":\"notLikeRight\",\"value\":\"mock\"}", Condition.class);
+        assertInstanceOf(LikeCondition.class, condition);
+        LikeCondition likeCondition = (LikeCondition) condition;
+        assertEquals("name", likeCondition.getField());
+        assertEquals("notLikeRight", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testBetweenCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"between\",\"value1\":\"100\",\"value2\":\"200\"}", Condition.class);
+        assertInstanceOf(BetweenCondition.class, condition);
+        BetweenCondition betweenCondition = (BetweenCondition) condition;
+        assertEquals("id", betweenCondition.getField());
+        assertEquals("between", betweenCondition.getOperator());
+        assertEquals("100", betweenCondition.getValue1());
+        assertEquals("200", betweenCondition.getValue2());
+    }
+
+    @Test
+    void testNotBetweenCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"notBetween\",\"value1\":\"100\",\"value2\":\"200\"}", Condition.class);
+        assertInstanceOf(BetweenCondition.class, condition);
+        BetweenCondition betweenCondition = (BetweenCondition) condition;
+        assertEquals("id", betweenCondition.getField());
+        assertEquals("notBetween", betweenCondition.getOperator());
+        assertEquals("100", betweenCondition.getValue1());
+        assertEquals("200", betweenCondition.getValue2());
+    }
+
+    @Test
+    void testNullCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"isNull\"}", Condition.class);
+        assertInstanceOf(NullCondition.class, condition);
+        NullCondition nullCondition = (NullCondition) condition;
+        assertEquals("id", nullCondition.getField());
+        assertEquals("isNull", nullCondition.getOperator());
+    }
+
+    @Test
+    void testNotNullCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"isNotNull\"}", Condition.class);
+        assertInstanceOf(NullCondition.class, condition);
+        NullCondition nullCondition = (NullCondition) condition;
+        assertEquals("id", nullCondition.getField());
+        assertEquals("isNotNull", nullCondition.getOperator());
+    }
+
+    @Test
+    void testInCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"in\",\"values\":[\"100\",\"101\"]}", Condition.class);
+        assertInstanceOf(InCondition.class, condition);
+        InCondition inCondition = (InCondition) condition;
+        assertEquals("id", inCondition.getField());
+        assertEquals("in", inCondition.getOperator());
+        assertEquals(2, inCondition.getValues().length);
+        assertEquals("100", inCondition.getValues()[0]);
+        assertEquals("101", inCondition.getValues()[1]);
+    }
+
+    @Test
+    void testNotInCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{\"field\":\"id\",\"operator\":\"notIn\",\"values\":[\"100\",\"101\"]}", Condition.class);
+        assertInstanceOf(InCondition.class, condition);
+        InCondition inCondition = (InCondition) condition;
+        assertEquals("id", inCondition.getField());
+        assertEquals("notIn", inCondition.getOperator());
+        assertEquals(2, inCondition.getValues().length);
+        assertEquals("100", inCondition.getValues()[0]);
+        assertEquals("101", inCondition.getValues()[1]);
+    }
+
+    @Test
+    void testCompositeAndCondition() throws JsonProcessingException {
         Condition condition = mapper.readValue("{" +
-            "\"operator\":\"or\"," +
-            "\"conditions\":[" +
+                "\"operator\":\"and\"," +
+                "\"conditions\":[" +
+                "{\"field\":\"id\",\"operator\":\"=\",\"value\":\"100\"}," +
+                "{\"field\":\"name\",\"operator\":\"like\",\"value\":\"mock\"}" +
+                "]" +
+                "}", Condition.class);
+
+        assertInstanceOf(CompositeCondition.class, condition);
+        CompositeCondition compositeCondition = (CompositeCondition) condition;
+        assertEquals("and", compositeCondition.getOperator());
+        assertEquals(2, compositeCondition.getConditions().length);
+
+        assertInstanceOf(GeneralCondition.class, compositeCondition.getConditions()[0]);
+        GeneralCondition generalCondition1 = (GeneralCondition) compositeCondition.getConditions()[0];
+        assertEquals("id", generalCondition1.getField());
+        assertEquals("=", generalCondition1.getOperator());
+        assertEquals("100", generalCondition1.getValue());
+
+        assertInstanceOf(LikeCondition.class, compositeCondition.getConditions()[1]);
+        LikeCondition likeCondition = (LikeCondition) compositeCondition.getConditions()[1];
+        assertEquals("name", likeCondition.getField());
+        assertEquals("like", likeCondition.getOperator());
+        assertEquals("mock", likeCondition.getValue());
+    }
+
+    @Test
+    void testCompositeOrCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{" +
+                "\"operator\":\"or\"," +
+                "\"conditions\":[" +
                 "{\"field\":\"id\",\"operator\":\"=\",\"value\":\"100\"}," +
                 "{\"field\":\"id\",\"operator\":\"=\",\"value\":\"101\"}" +
-            "]" +
-        "}", Condition.class);
+                "]" +
+                "}", Condition.class);
 
         assertInstanceOf(CompositeCondition.class, condition);
         CompositeCondition compositeCondition = (CompositeCondition) condition;
         assertEquals("or", compositeCondition.getOperator());
         assertEquals(2, compositeCondition.getConditions().length);
 
-        assertInstanceOf(SimpleCondition.class, compositeCondition.getConditions()[0]);
-        SimpleCondition condition1 = (SimpleCondition) compositeCondition.getConditions()[0];
-        assertEquals("id", condition1.getField());
-        assertEquals("=", condition1.getOperator());
-        assertEquals("100", condition1.getValue());
+        assertInstanceOf(GeneralCondition.class, compositeCondition.getConditions()[0]);
+        GeneralCondition generalCondition1 = (GeneralCondition) compositeCondition.getConditions()[0];
+        assertEquals("id", generalCondition1.getField());
+        assertEquals("=", generalCondition1.getOperator());
+        assertEquals("100", generalCondition1.getValue());
 
-        assertInstanceOf(SimpleCondition.class, compositeCondition.getConditions()[1]);
-        SimpleCondition condition2 = (SimpleCondition) compositeCondition.getConditions()[1];
-        assertEquals("id", condition2.getField());
-        assertEquals("=", condition2.getOperator());
-        assertEquals("101", condition2.getValue());
+        assertInstanceOf(GeneralCondition.class, compositeCondition.getConditions()[1]);
+        GeneralCondition generalCondition2 = (GeneralCondition) compositeCondition.getConditions()[1];
+        assertEquals("id", generalCondition2.getField());
+        assertEquals("=", generalCondition2.getOperator());
+        assertEquals("101", generalCondition2.getValue());
+    }
+
+    @Test
+    void testCompositeNotCondition() throws JsonProcessingException {
+        Condition condition = mapper.readValue("{" +
+                "\"operator\":\"not\"," +
+                "\"conditions\":[" +
+                "{\"field\":\"id\",\"operator\":\"=\",\"value\":\"100\"}" +
+                "]" +
+                "}", Condition.class);
+
+        assertInstanceOf(CompositeCondition.class, condition);
+        CompositeCondition compositeCondition = (CompositeCondition) condition;
+        assertEquals("not", compositeCondition.getOperator());
+
+        assertInstanceOf(GeneralCondition.class, compositeCondition.getConditions()[0]);
+        GeneralCondition generalCondition = (GeneralCondition) compositeCondition.getConditions()[0];
+        assertEquals("id", generalCondition.getField());
+        assertEquals("=", generalCondition.getOperator());
+        assertEquals("100", generalCondition.getValue());
     }
 }
