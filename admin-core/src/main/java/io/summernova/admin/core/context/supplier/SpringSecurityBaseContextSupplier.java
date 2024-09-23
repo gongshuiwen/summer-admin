1a1fc4eb0197a7b16628d25138f03123c4d74e5c
gongshuiwen <gongshuiwen@gmail.com>
2024-09-23 22:33:17 +0800

    [IMP] admin-core: optimize SpringSecurityBaseContextSupplier

--- admin-core/src/main/java/io/summernova/admin/core/context/supplier/SpringSecurityBaseContextSupplier.java
+++ admin-core/src/main/java/io/summernova/admin/core/context/supplier/SpringSecurityBaseContextSupplier.java
@@ -3,11 +3,8 @@ package io.summernova.admin.core.context.supplier;
 import io.summernova.admin.core.context.BaseContext;
 import io.summernova.admin.core.context.BaseContextImpl;
 import io.summernova.admin.core.security.account.BaseUser;
-import io.summernova.admin.core.util.SpringContextUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @author gongshuiwen
@@ -19,17 +16,8 @@ public class SpringSecurityBaseContextSupplier implements BaseContextSupplier {
         BaseContext baseContext = null;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated()) {
-            // normal case
             if (authentication.getPrincipal() instanceof BaseUser user) {
                 baseContext = new BaseContextImpl(user.getId());
-            }
-
-            // for testing
-            else if (authentication.getPrincipal() instanceof UserDetails userDetails) {
-                userDetails = SpringContextUtil.getBean(UserDetailsService.class)
-                        .loadUserByUsername(userDetails.getUsername());
-                if (userDetails instanceof BaseUser baseUser)
-                    baseContext = new BaseContextImpl(baseUser.getId());
             }
         }
 
